336e593f87a75d1dd54f90aecd92b968f4cb1b6b
BrunoDM2943 <bdm2943@gmail.com>
2015-08-10 23:51:38 -0300

    Alteração dos eventos

--- eventos.php
+++ eventos.php
@@ -5,42 +5,27 @@
 	</div>
 	<div class="panel-body">
 		<div class="list-group">
-			<a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Aula de música normal</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 08/08</p>
-				<p class="list-group-item-text">Horário: 14h00</p>				
-			</a> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Encontro de Mulheres</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 08/08</p>
-				<p class="list-group-item-text">Horário: 18h00</p>
-			</a> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Homenagem aos Pais</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Domingo - 09/08</p>
-				<p class="list-group-item-text">Horário: 19h00</p>
-			</a> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Ensaio do Coro</h4>
+<a href="#" class="list-group-item active">
+			<h4 class="list-group-item-heading">Ensaio do Coro</h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
 				<p class="list-group-item-text">Data: Sábado - 15/08</p>
 				<p class="list-group-item-text">Horário: 18h00</p>
-			</a> <a href="#" class="list-group-item active">
+			</a> <br> <a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Reunião de Jovens </h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
 				<p class="list-group-item-text">Data: Sábado - 22/08</p>
 				<p class="list-group-item-text">Horário: 16h00</p>
-			</a> <a href="#" class="list-group-item active">
+			</a> <br> <a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Ensaio do Coro</h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
 				<p class="list-group-item-text">Data: Sábado - 22/08</p>
 				<p class="list-group-item-text">Horário: 18h00</p>
-			</a> <a href="#" class="list-group-item active">
+			</a> <br> <a href="./pictures/aniversario_20_anos.jpg" class="list-group-item active" target="_blank">
 				<h4 class="list-group-item-heading">Culto das IEPs</h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
 				<p class="list-group-item-text">Data: Sábado - 29/08</p>
 				<p class="list-group-item-text">Horário: 18h00</p>
-			</a>
+			</a> <br>
 		</div>
 	</div>
 </div>
