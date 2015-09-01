e14aa39f756482c7db87d8a7f2a4b90c6d882da9
Bruno Damasceno Martins <bdm2943@gmail.com>
2015-09-01 09:12:47 -0300

    Atualização de Eventos

--- eventos.php
+++ eventos.php
@@ -6,30 +6,25 @@
 	<div class="panel-body">
 		<div class="list-group">
 <a href="#" class="list-group-item active">
-			<h4 class="list-group-item-heading">Ensaio do Coro</h4>
+			<h4 class="list-group-item-heading">Lnaçamento do filme "Você Acredita?"</h4>
+				<p class="list-group-item-text">Data: Sábado - 05/09</p>
+			</a> <br> <a href="#" class="list-group-item active">
+				<h4 class="list-group-item-heading">Aniversário da Igreja de São José dos Campos</h4>
+				<p class="list-group-item-text">Local: Igreja São José dos Campos</p>
+				<p class="list-group-item-text">Data: Sábado - 12/09</p>
+			</a> <br> <a href="#" class="list-group-item active">
+				<h4 class="list-group-item-heading">Ensaio do Teatro</h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 15/08</p>
+				<p class="list-group-item-text">Data: Sábado - 19/09</p>
 				<p class="list-group-item-text">Horário: 18h00</p>
 			</a> <br> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Reunião de Jovens </h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 22/08</p>
-				<p class="list-group-item-text">Horário: 16h00</p>
+				<h4 class="list-group-item-heading">Aniversário da Igreja de São José dos Campos</h4>
+				<p class="list-group-item-text">Local: Igreja São José dos Campos</p>
+				<p class="list-group-item-text">Data: Sábado - 26/09</p>
 			</a> <br> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Ensaio do Coro</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 22/08</p>
-				<p class="list-group-item-text">Horário: 18h00</p>
-			</a> <br> <a href="./pictures/aniversario_20_anos.jpg" class="list-group-item active" target="_blank">
-				<h4 class="list-group-item-heading">Culto das IEPs</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 29/08</p>
-				<p class="list-group-item-text">Horário: 18h00</p>
-			</a> <br> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Curso Bíblico</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Quarta - 26/08</p>
-				<p class="list-group-item-text">Horário: 20h00</p>
+				<h4 class="list-group-item-heading">Festival de Música</h4>
+				<p class="list-group-item-text">Local: IEP Sede</p>
+				<p class="list-group-item-text">Data: Sábado - 03/10</p>
 			</a> <br>
 		</div>
 	</div>
