8a9d91d962d882e1f8e2cf8908392ee1c6dd05b8
BrunoDM2943 <bdm2943@gmail.com>
2015-09-01 23:44:04 -0300

    Nova página de fotos

---
+++ fotos.php
@@ -0,0 +1,19 @@
+<?php include 'header.html';?>
+<script type="text/javascript">
+$( function() {
+    $( '#gallery' ).jGallery();
+} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Fotos</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+    <a href="fotos/linux.jpg"><img src="fotos/linux.jpg" alt="Linux" /></a>
+    <a href="fotos/jQuery.png"><img src="fotos/jQuery.png" alt="jQuery" /></
+</div>
+	</div>
+</div>
+</body>
+</html>
