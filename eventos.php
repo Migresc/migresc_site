68e030b0b3f7ee7856f7ce07f52c2fde051cf1cf
Guilherme <guilherme@reddrummer.com>
2015-09-04 10:53:55 -0300

    Update eventos.php

--- eventos.php
+++ eventos.php
@@ -19,7 +19,7 @@
 				<p class="list-group-item-text">Horário: 18h00</p>
 			</a> <br> <a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Aniversário da Igreja de Taboão da Serra</h4>
-				<p class="list-group-item-text">Local: Igreja Evangélica de Pinheiros em Taboão da Serra</p>
+				<p class="list-group-item-text">Local: Rua Hortência de Albuquerque Orlandino, 432, Pq. Pinheiros - Taboão da Serra - SP</p>
 				<p class="list-group-item-text">Data: Sábado - 26/09</p>
 			</a> <br> <a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Festival de Música</h4>
