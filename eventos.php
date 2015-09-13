3f6d0fdf4ec0f4c990b271143397c9b94038378b
Bruno Damasceno Martins <bdm2943@gmail.com>
2015-09-13 20:32:28 -0300

    Atualização dos Eventos

--- eventos.php
+++ eventos.php
@@ -5,14 +5,7 @@
 	</div>
 	<div class="panel-body">
 		<div class="list-group">
-<a href="#" class="list-group-item active">
-			<h4 class="list-group-item-heading">Lançamento do filme "Você Acredita?"</h4>
-				<p class="list-group-item-text">Data: Sábado - 05/09</p>
-			</a> <br> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Aniversário da Igreja de São José dos Campos</h4>
-				<p class="list-group-item-text">Local: Rua Marechal Floriano Peixoto, 570 - Centro - São José dos Campos - CEP: 12210-030</p>
-				<p class="list-group-item-text">Data: Sábado - 12/09</p>
-			</a> <br> <a href="#" class="list-group-item active">
+			<a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Aniversário da Igreja de Taboão da Serra</h4>
 				<p class="list-group-item-text">Local: Rua Hortência de Albuquerque Orlandino, 432, Pq. Pinheiros - Taboão da Serra - SP</p>
 				<p class="list-group-item-text">Data: Sábado - 26/09</p>
