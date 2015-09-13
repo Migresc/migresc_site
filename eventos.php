68a8e8fccb3373e9c7fe080be84db74414a43c90
Bruno Damasceno Martins <bdm2943@gmail.com>
2015-09-13 20:29:46 -0300

    Update eventos.php

--- eventos.php
+++ eventos.php
@@ -13,14 +13,10 @@
 				<p class="list-group-item-text">Local: Rua Marechal Floriano Peixoto, 570 - Centro - São José dos Campos - CEP: 12210-030</p>
 				<p class="list-group-item-text">Data: Sábado - 12/09</p>
 			</a> <br> <a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Ensaio do Teatro</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 19/09</p>
-				<p class="list-group-item-text">Horário: 18h00</p>
-			</a> <br> <a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Aniversário da Igreja de Taboão da Serra</h4>
 				<p class="list-group-item-text">Local: Rua Hortência de Albuquerque Orlandino, 432, Pq. Pinheiros - Taboão da Serra - SP</p>
 				<p class="list-group-item-text">Data: Sábado - 26/09</p>
+				<p class="list-group-item-text">Saída: 18h00</p>
 			</a> <br> <a href="#" class="list-group-item active">
 				<h4 class="list-group-item-heading">Festival de Música</h4>
 				<p class="list-group-item-text">Local: IEP Sede</p>
