92880d3ef8e2fd2da178cdb5dd05442c498e9c7d
BrunoDM2943 <bdm2943@gmail.com>
2015-09-24 23:28:30 -0300

    Nova tela de visitas

---
+++ visitas.php
@@ -0,0 +1,17 @@
+<?php include 'header.html';?>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Visitas</h3>
+	</div>
+	<div class="panel-body">
+		<h3>Irmãos, estamos realizando visitas! Os seguintes irmãos foram visitados recentemente:</h3>
+		<br>
+		<div class="list-group">
+		  <button type="button" class="list-group-item">Israel e Rita</button>
+		  <button type="button" class="list-group-item">Rosália</button>
+		</div>
+	</div>
+	<div class="panel-footer">Se algum irmão desejar receber uma visita, basta conversar com os pastores ou suas esposas.</div>
+</div>
+</body>
+</html>
