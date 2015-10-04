95266b9391d30a46009e7ce048ce89a75357c956
Bruno Damasceno Martins <bdm2943@gmail.com>
2015-10-04 20:44:38 -0300

    Atualização dos Eventos

--- eventos.php
+++ eventos.php
@@ -6,16 +6,22 @@
 	<div class="panel-body">
 		<div class="list-group">
 			<a href="#" class="list-group-item-active">
-				<h4 class="list-group-item-heading">Encontro de Namorados</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 03/10</p>
+				<h4 class="list-group-item-heading">Culto de Jovens</h4>
+				<p class="list-group-item-text">Local: Casa do Pr Claúdio </p>
+				<p class="list-group-item-text">Endereço: Rua Bento Ribeiro, 154</p>
+				<p class="list-group-item-text">Data: Sábado - 10/10</p>
+				<p class="list-group-item-text">Horário: 18:00</p>
+			</a> <a href="#" class="list-group-item-active">
+				<h4 class="list-group-item-heading">Aniversário do ministério de Pinheiros</h4>
+				<p class="list-group-item-text">Local: IEP SEDE</p>
+				<p class="list-group-item-text">Data: Sábado - 24/10</p>
 			</a> <br> <a href="./pcsp.php" class="list-group-item-active">
 				<h4 class="list-group-item-heading">PCSP</h4>
 				<p class="list-group-item-text">Local: OSESP</p>
 				<p class="list-group-item-text">Data: Sábado - 21/11</p>
 				<p class="list-group-item-text">Mais detalhes</p>
 				<span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
-			</a> <br>
+			</a> <br> 
 		</div>
 	</div>
 </div>
