f1fa06ec7b4a32d277b446efc27de37f7adf68e3
BrunoDM2943 <bdm2943@gmail.com>
2015-12-07 22:23:55 -0200

    Adição de rodapé

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 ﻿<?php include 'header.html';?>
-<h1>Seja bem vindo! Para navegar, basta pressionar em algum link no menu acima!</h1>
-</body>
-</html>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">Seja bem vindo!</h3>
+    </div>
+    <div class="panel-body">
+        <h3>Para navegar, basta pressionar em algum link no menu acima!</h3>
+    </div>
+</div>
+<?php include 'footer.html';?>
