94e6774ce0fe1844de1b55261d3d0d2d51f53313
Bruno Damasceno Martins <bdm2943@gmail.com>
2015-12-13 20:39:40 -0200

    Atulização
    Foi atualizado os eventos e corrigido um bug na tela de Culto Online

--- eventos.php
+++ eventos.php
@@ -8,13 +8,40 @@
 			<br> <a href="#" class="list-group-item-active">
 				<h4 class="list-group-item-heading">Ensaio do Teatro e do Coral</h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 12/12</p>
-			</a> <br> 
+				<p class="list-group-item-text">Data: Sábado - 19/12</p>
+			</a>
+			<br> <a href="#" class="list-group-item-active">
+				<h4 class="list-group-item-heading">Batismos</h4>
+				<p class="list-group-item-text">Local: IEPEM</p>
+				<p class="list-group-item-text">Data: Domingo - 20/12</p>
+			</a>
 			<br> <a href="#" class="list-group-item-active">
 				<h4 class="list-group-item-heading">Ensaio do Teatro e do Coral</h4>
 				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 19/12</p>
-			</a> <br> 
+				<p class="list-group-item-text">Data: Sábado - 26/12</p>
+			</a>
+			<br> <a href="#" class="list-group-item-active">
+				<h4 class="list-group-item-heading">Amigro Secreto</h4>
+				<p class="list-group-item-text">Local: IEPEM</p>
+				<p class="list-group-item-text">Data: Sábado - 26/12</p>
+			</a>
+			<br> <a href="#" class="list-group-item-active">
+				<h4 class="list-group-item-heading">Apresentação da Cantata</h4>
+				<p class="list-group-item-text">Local: IEPEM</p>
+				<p class="list-group-item-text">Data: Domingo - 27/12</p>
+			</a>
+			<br> <a href="#" class="list-group-item-active">
+				<h4 class="list-group-item-heading">Culto de Encerramento do Ano</h4>
+				<p class="list-group-item-text">Local: IEPEM</p>
+				<p class="list-group-item-text">Data: Domingo - 31/12</p>
+				<p class="list-group-item-text">Horário: 20h</p>
+			</a>
+			<br> <a href="#" class="list-group-item-active">
+				<h4 class="list-group-item-heading">Jantar</h4>
+				<p class="list-group-item-text">Local: IEPEM</p>
+				<p class="list-group-item-text">Data: Domingo - 31/12</p>
+				<p class="list-group-item-text">Horário: 22h</p>
+			</a>
 		</div>
 	</div>
 </div>
