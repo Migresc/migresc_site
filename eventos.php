33d3efe520659943a56fcadad12a9ad89befd6fd
BrunoDM2943 <bdm2943@gmail.com>
2016-01-05 19:47:03 -0200

    Atualização dos eventos

--- eventos.php
+++ eventos.php
@@ -6,15 +6,10 @@
 	<div class="panel-body">
 		<div class="list-group">
 			<br> <a href="#" class="list-group-item-active">
-				<h4 class="list-group-item-heading">Ensaio do Teatro e do Coral</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 12/12</p>
-			</a> <br> 
-			<br> <a href="#" class="list-group-item-active">
-				<h4 class="list-group-item-heading">Ensaio do Teatro e do Coral</h4>
-				<p class="list-group-item-text">Local: IEPEM</p>
-				<p class="list-group-item-text">Data: Sábado - 19/12</p>
-			</a> <br> 
+			<h4 class="list-group-item-heading">Culto de Jovens</h4>
+			<p class="list-group-item-text">Local: IEPEM</p>
+			<p class="list-group-item-text">Data: Sábado - 09/01</p>
+		</a> <br>
 		</div>
 	</div>
 </div>
