35b30a96448b73fee7932b5c34c464a27fe0a59e
BrunoDM2943 <bdm2943@gmail.com>
2016-01-11 09:04:59 -0200

    Atualização da home

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
         <h3 class="panel-title">Seja bem vindo!</h3>
     </div>
     <div class="panel-body">
-        <h3>Para navegar, basta pressionar em algum link no menu acima!</h3>
+        <h3>Retire aqui a ficha de inscrição para o nosso Retiro 2016!</h3>
+        <p> <a href="./arquivos/fichaRetiro2016.pdf" class="glyphicon glyphicon-download"> </a></p>
     </div>
 </div>
 <?php include 'footer.html';?>
