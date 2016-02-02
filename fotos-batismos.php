90b6e268c8e53b01283fffacf458b483f0def3a8
BrunoDM2943 <bdm2943@gmail.com>
2016-02-02 21:47:12 -0200

    Fotos do Batismo 2015

---
+++ fotos-batismos.php
@@ -0,0 +1,137 @@
+<?php include 'header.html';?>
+<script type="text/javascript">
+	$( function() {
+		$( '#gallery' ).jGallery();
+	} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Fotos do Batismo</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+			<a href="fotos/2015/Batismos/1.JPG"> <img src="fotos/2015/Batismos/1.JPG" alt="1"/></a>
+			<a href="fotos/2015/Batismos/2.JPG"> <img src="fotos/2015/Batismos/2.JPG" alt="2"/></a>
+			<a href="fotos/2015/Batismos/3.JPG"> <img src="fotos/2015/Batismos/3.JPG" alt="3"/></a>
+			<a href="fotos/2015/Batismos/4.JPG"> <img src="fotos/2015/Batismos/4.JPG" alt="4"/></a>
+			<a href="fotos/2015/Batismos/5.JPG"> <img src="fotos/2015/Batismos/5.JPG" alt="5"/></a>
+			<a href="fotos/2015/Batismos/6.JPG"> <img src="fotos/2015/Batismos/6.JPG" alt="6"/></a>
+			<a href="fotos/2015/Batismos/7.JPG"> <img src="fotos/2015/Batismos/7.JPG" alt="7"/></a>
+			<a href="fotos/2015/Batismos/8.JPG"> <img src="fotos/2015/Batismos/8.JPG" alt="8"/></a>
+			<a href="fotos/2015/Batismos/9.JPG"> <img src="fotos/2015/Batismos/9.JPG" alt="9"/></a>
+			<a href="fotos/2015/Batismos/10.JPG"><img src="fotos/2015/Batismos/10.JPG" alt="10"/></a>
+			<a href="fotos/2015/Batismos/11.JPG"><img src="fotos/2015/Batismos/11.JPG" alt="11"/></a>
+			<a href="fotos/2015/Batismos/12.JPG"><img src="fotos/2015/Batismos/12.JPG" alt="12"/></a>
+			<a href="fotos/2015/Batismos/13.JPG"><img src="fotos/2015/Batismos/13.JPG" alt="13"/></a>
+			<a href="fotos/2015/Batismos/14.JPG"><img src="fotos/2015/Batismos/14.JPG" alt="14"/></a>
+			<a href="fotos/2015/Batismos/15.JPG"><img src="fotos/2015/Batismos/15.JPG" alt="15"/></a>
+			<a href="fotos/2015/Batismos/16.JPG"><img src="fotos/2015/Batismos/16.JPG" alt="16"/></a>
+			<a href="fotos/2015/Batismos/17.JPG"><img src="fotos/2015/Batismos/17.JPG" alt="17"/></a>
+			<a href="fotos/2015/Batismos/18.JPG"><img src="fotos/2015/Batismos/18.JPG" alt="18"/></a>
+			<a href="fotos/2015/Batismos/19.JPG"><img src="fotos/2015/Batismos/19.JPG" alt="19"/></a>
+			<a href="fotos/2015/Batismos/20.JPG"><img src="fotos/2015/Batismos/20.JPG" alt="20"/></a>
+			<a href="fotos/2015/Batismos/21.JPG"><img src="fotos/2015/Batismos/21.JPG" alt="21"/></a>
+			<a href="fotos/2015/Batismos/22.JPG"><img src="fotos/2015/Batismos/22.JPG" alt="22"/></a>
+			<a href="fotos/2015/Batismos/23.JPG"><img src="fotos/2015/Batismos/23.JPG" alt="23"/></a>
+			<a href="fotos/2015/Batismos/24.JPG"><img src="fotos/2015/Batismos/24.JPG" alt="24"/></a>
+			<a href="fotos/2015/Batismos/25.JPG"><img src="fotos/2015/Batismos/25.JPG" alt="25"/></a>
+			<a href="fotos/2015/Batismos/26.JPG"><img src="fotos/2015/Batismos/26.JPG" alt="26"/></a>
+			<a href="fotos/2015/Batismos/27.JPG"><img src="fotos/2015/Batismos/27.JPG" alt="27"/></a>
+			<a href="fotos/2015/Batismos/28.JPG"><img src="fotos/2015/Batismos/28.JPG" alt="28"/></a>
+			<a href="fotos/2015/Batismos/29.JPG"><img src="fotos/2015/Batismos/29.JPG" alt="29"/></a>
+			<a href="fotos/2015/Batismos/30.JPG"><img src="fotos/2015/Batismos/30.JPG" alt="30"/></a>
+			<a href="fotos/2015/Batismos/31.JPG"><img src="fotos/2015/Batismos/31.JPG" alt="31"/></a>
+			<a href="fotos/2015/Batismos/32.JPG"><img src="fotos/2015/Batismos/32.JPG" alt="32"/></a>
+			<a href="fotos/2015/Batismos/33.JPG"><img src="fotos/2015/Batismos/33.JPG" alt="33"/></a>
+			<a href="fotos/2015/Batismos/34.JPG"><img src="fotos/2015/Batismos/34.JPG" alt="34"/></a>
+			<a href="fotos/2015/Batismos/35.JPG"><img src="fotos/2015/Batismos/35.JPG" alt="35"/></a>
+			<a href="fotos/2015/Batismos/36.JPG"><img src="fotos/2015/Batismos/36.JPG" alt="36"/></a>
+			<a href="fotos/2015/Batismos/37.JPG"><img src="fotos/2015/Batismos/37.JPG" alt="37"/></a>
+			<a href="fotos/2015/Batismos/38.JPG"><img src="fotos/2015/Batismos/38.JPG" alt="38"/></a>
+			<a href="fotos/2015/Batismos/39.JPG"><img src="fotos/2015/Batismos/39.JPG" alt="39"/></a>
+			<a href="fotos/2015/Batismos/40.JPG"><img src="fotos/2015/Batismos/40.JPG" alt="40"/></a>
+			<a href="fotos/2015/Batismos/41.JPG"><img src="fotos/2015/Batismos/41.JPG" alt="41"/></a>
+			<a href="fotos/2015/Batismos/42.JPG"><img src="fotos/2015/Batismos/42.JPG" alt="42"/></a>
+			<a href="fotos/2015/Batismos/43.JPG"><img src="fotos/2015/Batismos/43.JPG" alt="43"/></a>
+			<a href="fotos/2015/Batismos/44.JPG"><img src="fotos/2015/Batismos/44.JPG" alt="44"/></a>
+			<a href="fotos/2015/Batismos/45.JPG"><img src="fotos/2015/Batismos/45.JPG" alt="45"/></a>
+			<a href="fotos/2015/Batismos/46.JPG"><img src="fotos/2015/Batismos/46.JPG" alt="46"/></a>
+			<a href="fotos/2015/Batismos/47.JPG"><img src="fotos/2015/Batismos/47.JPG" alt="47"/></a>
+			<a href="fotos/2015/Batismos/48.JPG"> <img src="fotos/2015/Batismos/48.JPG" alt="48"/></a>
+			<a href="fotos/2015/Batismos/49.JPG"> <img src="fotos/2015/Batismos/49.JPG" alt="49"/></a>
+			<a href="fotos/2015/Batismos/50.JPG"> <img src="fotos/2015/Batismos/50.JPG" alt="50"/></a>
+			<a href="fotos/2015/Batismos/51.JPG"> <img src="fotos/2015/Batismos/51.JPG" alt="51"/></a>
+			<a href="fotos/2015/Batismos/52.JPG"> <img src="fotos/2015/Batismos/52.JPG" alt="52"/></a>
+			<a href="fotos/2015/Batismos/53.JPG"> <img src="fotos/2015/Batismos/53.JPG" alt="53"/></a>
+			<a href="fotos/2015/Batismos/54.JPG"> <img src="fotos/2015/Batismos/54.JPG" alt="54"/></a>
+			<a href="fotos/2015/Batismos/55.JPG"> <img src="fotos/2015/Batismos/55.JPG" alt="55"/></a>
+			<a href="fotos/2015/Batismos/56.JPG"> <img src="fotos/2015/Batismos/56.JPG" alt="56"/></a>
+			<a href="fotos/2015/Batismos/57.JPG"><img src="fotos/2015/Batismos/57.JPG" alt="57"/></a>
+			<a href="fotos/2015/Batismos/58.JPG"><img src="fotos/2015/Batismos/58.JPG" alt="58"/></a>
+			<a href="fotos/2015/Batismos/59.JPG"><img src="fotos/2015/Batismos/59.JPG" alt="59"/></a>
+			<a href="fotos/2015/Batismos/60.JPG"><img src="fotos/2015/Batismos/60.JPG" alt="60"/></a>
+			<a href="fotos/2015/Batismos/61.JPG"><img src="fotos/2015/Batismos/61.JPG" alt="61"/></a>
+			<a href="fotos/2015/Batismos/62.JPG"><img src="fotos/2015/Batismos/62.JPG" alt="62"/></a>
+			<a href="fotos/2015/Batismos/63.JPG"><img src="fotos/2015/Batismos/63.JPG" alt="63"/></a>
+			<a href="fotos/2015/Batismos/64.JPG"><img src="fotos/2015/Batismos/64.JPG" alt="64"/></a>
+			<a href="fotos/2015/Batismos/65.JPG"><img src="fotos/2015/Batismos/65.JPG" alt="65"/></a>
+			<a href="fotos/2015/Batismos/66.JPG"><img src="fotos/2015/Batismos/66.JPG" alt="66"/></a>
+			<a href="fotos/2015/Batismos/67.JPG"><img src="fotos/2015/Batismos/67.JPG" alt="67"/></a>
+			<a href="fotos/2015/Batismos/68.JPG"><img src="fotos/2015/Batismos/68.JPG" alt="68"/></a>
+			<a href="fotos/2015/Batismos/69.JPG"><img src="fotos/2015/Batismos/69.JPG" alt="69"/></a>
+			<a href="fotos/2015/Batismos/70.JPG"><img src="fotos/2015/Batismos/70.JPG" alt="70"/></a>
+			<a href="fotos/2015/Batismos/71.JPG"><img src="fotos/2015/Batismos/71.JPG" alt="71"/></a>
+			<a href="fotos/2015/Batismos/72.JPG"><img src="fotos/2015/Batismos/72.JPG" alt="72"/></a>
+			<a href="fotos/2015/Batismos/73.JPG"><img src="fotos/2015/Batismos/73.JPG" alt="73"/></a>
+			<a href="fotos/2015/Batismos/74.JPG"><img src="fotos/2015/Batismos/74.JPG" alt="74"/></a>
+			<a href="fotos/2015/Batismos/75.JPG"><img src="fotos/2015/Batismos/75.JPG" alt="75"/></a>
+			<a href="fotos/2015/Batismos/76.JPG"><img src="fotos/2015/Batismos/76.JPG" alt="76"/></a>
+			<a href="fotos/2015/Batismos/77.JPG"><img src="fotos/2015/Batismos/77.JPG" alt="77"/></a>
+			<a href="fotos/2015/Batismos/78.JPG"><img src="fotos/2015/Batismos/78.JPG" alt="78"/></a>
+			<a href="fotos/2015/Batismos/79.JPG"><img src="fotos/2015/Batismos/79.JPG" alt="79"/></a>
+			<a href="fotos/2015/Batismos/80.JPG"><img src="fotos/2015/Batismos/80.JPG" alt="80"/></a>
+			<a href="fotos/2015/Batismos/81.JPG"><img src="fotos/2015/Batismos/81.JPG" alt="81"/></a>
+			<a href="fotos/2015/Batismos/82.JPG"><img src="fotos/2015/Batismos/82.JPG" alt="82"/></a>
+			<a href="fotos/2015/Batismos/83.JPG"><img src="fotos/2015/Batismos/83.JPG" alt="83"/></a>
+			<a href="fotos/2015/Batismos/84.JPG"><img src="fotos/2015/Batismos/84.JPG" alt="84"/></a>
+			<a href="fotos/2015/Batismos/85.JPG"><img src="fotos/2015/Batismos/85.JPG" alt="85"/></a>
+			<a href="fotos/2015/Batismos/86.JPG"><img src="fotos/2015/Batismos/86.JPG" alt="86"/></a>
+			<a href="fotos/2015/Batismos/87.JPG"><img src="fotos/2015/Batismos/87.JPG" alt="87"/></a>
+			<a href="fotos/2015/Batismos/88.JPG"><img src="fotos/2015/Batismos/88.JPG" alt="88"/></a>
+			<a href="fotos/2015/Batismos/89.JPG"><img src="fotos/2015/Batismos/89.JPG" alt="89"/></a>
+			<a href="fotos/2015/Batismos/90.JPG"><img src="fotos/2015/Batismos/90.JPG" alt="90"/></a>
+			<a href="fotos/2015/Batismos/91.JPG"><img src="fotos/2015/Batismos/91.JPG" alt="91"/></a>
+			<a href="fotos/2015/Batismos/92.JPG"><img src="fotos/2015/Batismos/92.JPG" alt="92"/></a>
+			<a href="fotos/2015/Batismos/93.JPG"><img src="fotos/2015/Batismos/93.JPG" alt="93"/></a>
+			<a href="fotos/2015/Batismos/94.JPG"><img src="fotos/2015/Batismos/94.JPG" alt="94"/></a>
+			<a href="fotos/2015/Batismos/95.JPG"> <img src="fotos/2015/Batismos/95.JPG" alt="95"/></a>
+			<a href="fotos/2015/Batismos/96.JPG"> <img src="fotos/2015/Batismos/96.JPG" alt="96"/></a>
+			<a href="fotos/2015/Batismos/97.JPG"> <img src="fotos/2015/Batismos/97.JPG" alt="97"/></a>
+			<a href="fotos/2015/Batismos/98.JPG"> <img src="fotos/2015/Batismos/98.JPG" alt="98"/></a>
+			<a href="fotos/2015/Batismos/99.JPG"> <img src="fotos/2015/Batismos/99.JPG" alt="99"/></a>
+			<a href="fotos/2015/Batismos/100.JPG"> <img src="fotos/2015/Batismos/100.JPG" alt="100"/></a>
+			<a href="fotos/2015/Batismos/101.JPG"> <img src="fotos/2015/Batismos/101.JPG" alt="101"/></a>
+			<a href="fotos/2015/Batismos/102.JPG"> <img src="fotos/2015/Batismos/102.JPG" alt="102"/></a>
+			<a href="fotos/2015/Batismos/103.JPG"> <img src="fotos/2015/Batismos/103.JPG" alt="103"/></a>
+			<a href="fotos/2015/Batismos/104.JPG"><img src="fotos/2015/Batismos/104.JPG" alt="104"/></a>
+			<a href="fotos/2015/Batismos/105.JPG"><img src="fotos/2015/Batismos/105.JPG" alt="105"/></a>
+			<a href="fotos/2015/Batismos/106.JPG"><img src="fotos/2015/Batismos/106.JPG" alt="106"/></a>
+			<a href="fotos/2015/Batismos/107.JPG"><img src="fotos/2015/Batismos/107.JPG" alt="107"/></a>
+			<a href="fotos/2015/Batismos/108.JPG"><img src="fotos/2015/Batismos/108.JPG" alt="108"/></a>
+			<a href="fotos/2015/Batismos/109.JPG"><img src="fotos/2015/Batismos/109.JPG" alt="109"/></a>
+			<a href="fotos/2015/Batismos/110.JPG"><img src="fotos/2015/Batismos/110.JPG" alt="110"/></a>
+			<a href="fotos/2015/Batismos/111.JPG"><img src="fotos/2015/Batismos/111.JPG" alt="111"/></a>
+			<a href="fotos/2015/Batismos/112.JPG"><img src="fotos/2015/Batismos/112.JPG" alt="112"/></a>
+			<a href="fotos/2015/Batismos/113.JPG"><img src="fotos/2015/Batismos/113.JPG" alt="113"/></a>
+			<a href="fotos/2015/Batismos/fotos/2015/Batismos/114.JPG"><img src="fotos/2015/Batismos/fotos/2015/Batismos/114.JPG" alt="114"/></a>
+			<a href="fotos/2015/Batismos/fotos/2015/Batismos/115.JPG"><img src="fotos/2015/Batismos/fotos/2015/Batismos/115.JPG" alt="115"/></a>
+			<a href="fotos/2015/Batismos/fotos/2015/Batismos/116.JPG"><img src="fotos/2015/Batismos/fotos/2015/Batismos/116.JPG" alt="116"/></a>
+			<a href="fotos/2015/Batismos/fotos/2015/Batismos/117.JPG"><img src="fotos/2015/Batismos/fotos/2015/Batismos/117.JPG" alt="117"/></a>
+			<a href="fotos/2015/Batismos/fotos/2015/Batismos/118.JPG"><img src="fotos/2015/Batismos/fotos/2015/Batismos/118.JPG" alt="118"/></a>
+			<a href="fotos/2015/Batismos/fotos/2015/Batismos/119.JPG"><img src="fotos/2015/Batismos/fotos/2015/Batismos/119.JPG" alt="119"/></a>
+			<a href="fotos/2015/Batismos/120.JPG"><img src="fotos/2015/Batismos/120.JPG" alt="120"/></a>
+
+		</div>
+	</div>
+</div>
+<?php include 'footer.html';?>
