b3ff1ce1add4b20bfcf1473a26a49217a0c97f9c
BrunoDM2943 <bdm2943@gmail.com>
2016-02-02 22:23:34 -0200

    Fotos da Cantata de Natal 2015

---
+++ fotos-natal.php
@@ -0,0 +1,102 @@
+<?php include 'header.html';?>
+<script type="text/javascript">
+	$( function() {
+		$( '#gallery' ).jGallery();
+	} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Fotos da Cantata de Natal</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+			<a href="fotos/2015/Natal/1.jpg"> <img src="fotos/2015/Natal/1.jpg" alt="1"/></a>
+			<a href="fotos/2015/Natal/2.jpg"> <img src="fotos/2015/Natal/2.jpg" alt="2"/></a>
+			<a href="fotos/2015/Natal/3.jpg"> <img src="fotos/2015/Natal/3.jpg" alt="3"/></a>
+			<a href="fotos/2015/Natal/4.jpg"> <img src="fotos/2015/Natal/4.jpg" alt="4"/></a>
+			<a href="fotos/2015/Natal/5.jpg"> <img src="fotos/2015/Natal/5.jpg" alt="5"/></a>
+			<a href="fotos/2015/Natal/6.jpg"> <img src="fotos/2015/Natal/6.jpg" alt="6"/></a>
+			<a href="fotos/2015/Natal/7.jpg"> <img src="fotos/2015/Natal/7.jpg" alt="7"/></a>
+			<a href="fotos/2015/Natal/8.jpg"> <img src="fotos/2015/Natal/8.jpg" alt="8"/></a>
+			<a href="fotos/2015/Natal/9.jpg"> <img src="fotos/2015/Natal/9.jpg" alt="9"/></a>
+			<a href="fotos/2015/Natal/10.jpg"><img src="fotos/2015/Natal/10.jpg" alt="10"/></a>
+			<a href="fotos/2015/Natal/11.jpg"><img src="fotos/2015/Natal/11.jpg" alt="11"/></a>
+			<a href="fotos/2015/Natal/12.jpg"><img src="fotos/2015/Natal/12.jpg" alt="12"/></a>
+			<a href="fotos/2015/Natal/13.jpg"><img src="fotos/2015/Natal/13.jpg" alt="13"/></a>
+			<a href="fotos/2015/Natal/14.jpg"><img src="fotos/2015/Natal/14.jpg" alt="14"/></a>
+			<a href="fotos/2015/Natal/15.jpg"><img src="fotos/2015/Natal/15.jpg" alt="15"/></a>
+			<a href="fotos/2015/Natal/16.jpg"><img src="fotos/2015/Natal/16.jpg" alt="16"/></a>
+			<a href="fotos/2015/Natal/17.jpg"><img src="fotos/2015/Natal/17.jpg" alt="17"/></a>
+			<a href="fotos/2015/Natal/18.jpg"><img src="fotos/2015/Natal/18.jpg" alt="18"/></a>
+			<a href="fotos/2015/Natal/19.jpg"><img src="fotos/2015/Natal/19.jpg" alt="19"/></a>
+			<a href="fotos/2015/Natal/20.jpg"><img src="fotos/2015/Natal/20.jpg" alt="20"/></a>
+			<a href="fotos/2015/Natal/21.jpg"><img src="fotos/2015/Natal/21.jpg" alt="21"/></a>
+			<a href="fotos/2015/Natal/22.jpg"><img src="fotos/2015/Natal/22.jpg" alt="22"/></a>
+			<a href="fotos/2015/Natal/23.jpg"><img src="fotos/2015/Natal/23.jpg" alt="23"/></a>
+			<a href="fotos/2015/Natal/24.jpg"><img src="fotos/2015/Natal/24.jpg" alt="24"/></a>
+			<a href="fotos/2015/Natal/25.jpg"><img src="fotos/2015/Natal/25.jpg" alt="25"/></a>
+			<a href="fotos/2015/Natal/26.jpg"><img src="fotos/2015/Natal/26.jpg" alt="26"/></a>
+			<a href="fotos/2015/Natal/27.jpg"><img src="fotos/2015/Natal/27.jpg" alt="27"/></a>
+			<a href="fotos/2015/Natal/28.jpg"><img src="fotos/2015/Natal/28.jpg" alt="28"/></a>
+			<a href="fotos/2015/Natal/29.jpg"><img src="fotos/2015/Natal/29.jpg" alt="29"/></a>
+			<a href="fotos/2015/Natal/30.jpg"><img src="fotos/2015/Natal/30.jpg" alt="30"/></a>
+			<a href="fotos/2015/Natal/31.jpg"><img src="fotos/2015/Natal/31.jpg" alt="31"/></a>
+			<a href="fotos/2015/Natal/32.jpg"><img src="fotos/2015/Natal/32.jpg" alt="32"/></a>
+			<a href="fotos/2015/Natal/33.jpg"><img src="fotos/2015/Natal/33.jpg" alt="33"/></a>
+			<a href="fotos/2015/Natal/34.jpg"><img src="fotos/2015/Natal/34.jpg" alt="34"/></a>
+			<a href="fotos/2015/Natal/35.jpg"><img src="fotos/2015/Natal/35.jpg" alt="35"/></a>
+			<a href="fotos/2015/Natal/36.jpg"><img src="fotos/2015/Natal/36.jpg" alt="36"/></a>
+			<a href="fotos/2015/Natal/37.jpg"><img src="fotos/2015/Natal/37.jpg" alt="37"/></a>
+			<a href="fotos/2015/Natal/38.jpg"><img src="fotos/2015/Natal/38.jpg" alt="38"/></a>
+			<a href="fotos/2015/Natal/39.jpg"><img src="fotos/2015/Natal/39.jpg" alt="39"/></a>
+			<a href="fotos/2015/Natal/40.jpg"><img src="fotos/2015/Natal/40.jpg" alt="40"/></a>
+			<a href="fotos/2015/Natal/41.jpg"><img src="fotos/2015/Natal/41.jpg" alt="41"/></a>
+			<a href="fotos/2015/Natal/42.jpg"><img src="fotos/2015/Natal/42.jpg" alt="42"/></a>
+			<a href="fotos/2015/Natal/43.jpg"><img src="fotos/2015/Natal/43.jpg" alt="43"/></a>
+			<a href="fotos/2015/Natal/44.jpg"><img src="fotos/2015/Natal/44.jpg" alt="44"/></a>
+			<a href="fotos/2015/Natal/45.jpg"><img src="fotos/2015/Natal/45.jpg" alt="45"/></a>
+			<a href="fotos/2015/Natal/46.jpg"><img src="fotos/2015/Natal/46.jpg" alt="46"/></a>
+			<a href="fotos/2015/Natal/47.jpg"><img src="fotos/2015/Natal/47.jpg" alt="47"/></a>
+			<a href="fotos/2015/Natal/48.jpg"> <img src="fotos/2015/Natal/48.jpg" alt="48"/></a>
+			<a href="fotos/2015/Natal/49.jpg"> <img src="fotos/2015/Natal/49.jpg" alt="49"/></a>
+			<a href="fotos/2015/Natal/50.jpg"> <img src="fotos/2015/Natal/50.jpg" alt="50"/></a>
+			<a href="fotos/2015/Natal/51.jpg"> <img src="fotos/2015/Natal/51.jpg" alt="51"/></a>
+			<a href="fotos/2015/Natal/52.jpg"> <img src="fotos/2015/Natal/52.jpg" alt="52"/></a>
+			<a href="fotos/2015/Natal/53.jpg"> <img src="fotos/2015/Natal/53.jpg" alt="53"/></a>
+			<a href="fotos/2015/Natal/54.jpg"> <img src="fotos/2015/Natal/54.jpg" alt="54"/></a>
+			<a href="fotos/2015/Natal/55.jpg"> <img src="fotos/2015/Natal/55.jpg" alt="55"/></a>
+			<a href="fotos/2015/Natal/56.jpg"> <img src="fotos/2015/Natal/56.jpg" alt="56"/></a>
+			<a href="fotos/2015/Natal/57.jpg"><img src="fotos/2015/Natal/57.jpg" alt="57"/></a>
+			<a href="fotos/2015/Natal/58.jpg"><img src="fotos/2015/Natal/58.jpg" alt="58"/></a>
+			<a href="fotos/2015/Natal/59.jpg"><img src="fotos/2015/Natal/59.jpg" alt="59"/></a>
+			<a href="fotos/2015/Natal/60.jpg"><img src="fotos/2015/Natal/60.jpg" alt="60"/></a>
+			<a href="fotos/2015/Natal/61.jpg"><img src="fotos/2015/Natal/61.jpg" alt="61"/></a>
+			<a href="fotos/2015/Natal/62.jpg"><img src="fotos/2015/Natal/62.jpg" alt="62"/></a>
+			<a href="fotos/2015/Natal/63.jpg"><img src="fotos/2015/Natal/63.jpg" alt="63"/></a>
+			<a href="fotos/2015/Natal/64.jpg"><img src="fotos/2015/Natal/64.jpg" alt="64"/></a>
+			<a href="fotos/2015/Natal/65.jpg"><img src="fotos/2015/Natal/65.jpg" alt="65"/></a>
+			<a href="fotos/2015/Natal/66.jpg"><img src="fotos/2015/Natal/66.jpg" alt="66"/></a>
+			<a href="fotos/2015/Natal/67.jpg"><img src="fotos/2015/Natal/67.jpg" alt="67"/></a>
+			<a href="fotos/2015/Natal/68.jpg"><img src="fotos/2015/Natal/68.jpg" alt="68"/></a>
+			<a href="fotos/2015/Natal/69.jpg"><img src="fotos/2015/Natal/69.jpg" alt="69"/></a>
+			<a href="fotos/2015/Natal/70.jpg"><img src="fotos/2015/Natal/70.jpg" alt="70"/></a>
+			<a href="fotos/2015/Natal/71.jpg"><img src="fotos/2015/Natal/71.jpg" alt="71"/></a>
+			<a href="fotos/2015/Natal/72.jpg"><img src="fotos/2015/Natal/72.jpg" alt="72"/></a>
+			<a href="fotos/2015/Natal/73.jpg"><img src="fotos/2015/Natal/73.jpg" alt="73"/></a>
+			<a href="fotos/2015/Natal/74.jpg"><img src="fotos/2015/Natal/74.jpg" alt="74"/></a>
+			<a href="fotos/2015/Natal/75.jpg"><img src="fotos/2015/Natal/75.jpg" alt="75"/></a>
+			<a href="fotos/2015/Natal/76.jpg"><img src="fotos/2015/Natal/76.jpg" alt="76"/></a>
+			<a href="fotos/2015/Natal/77.jpg"><img src="fotos/2015/Natal/77.jpg" alt="77"/></a>
+			<a href="fotos/2015/Natal/78.jpg"><img src="fotos/2015/Natal/78.jpg" alt="78"/></a>
+			<a href="fotos/2015/Natal/79.jpg"><img src="fotos/2015/Natal/79.jpg" alt="79"/></a>
+			<a href="fotos/2015/Natal/80.jpg"><img src="fotos/2015/Natal/80.jpg" alt="80"/></a>
+			<a href="fotos/2015/Natal/81.jpg"><img src="fotos/2015/Natal/81.jpg" alt="81"/></a>
+			<a href="fotos/2015/Natal/82.jpg"><img src="fotos/2015/Natal/82.jpg" alt="82"/></a>
+			<a href="fotos/2015/Natal/83.jpg"><img src="fotos/2015/Natal/83.jpg" alt="83"/></a>
+			<a href="fotos/2015/Natal/84.jpg"><img src="fotos/2015/Natal/84.jpg" alt="84"/></a>
+			<a href="fotos/2015/Natal/85.jpg"><img src="fotos/2015/Natal/85.jpg" alt="85"/></a>
+			<a href="fotos/2015/Natal/86.jpg"><img src="fotos/2015/Natal/86.jpg" alt="86"/></a>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html';?>
