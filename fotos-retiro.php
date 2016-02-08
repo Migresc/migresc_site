d73254e140d5e5a96004d388259ed3e029d41549
Bruno Damasceno Martins <bdm2943@gmail.com>
2016-02-08 11:33:42 -0200

    Atualização
    Fotos do Retiro 2016

---
+++ fotos-retiro.php
@@ -0,0 +1,193 @@
+<?php include 'header.html';?>
+<script type="text/javascript">
+	$( function() {
+		$( '#gallery' ).jGallery();
+	} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Fotos do Retiro 2016</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+			<a href="fotos/2016/retiro/01.JPG"> <img src="fotos/2016/retiro/01.JPG" alt="01"/></a>
+			<a href="fotos/2016/retiro/02.JPG"> <img src="fotos/2016/retiro/02.JPG" alt="02"/></a>
+			<a href="fotos/2016/retiro/03.JPG"> <img src="fotos/2016/retiro/03.JPG" alt="03"/></a>
+			<a href="fotos/2016/retiro/04.JPG"> <img src="fotos/2016/retiro/04.JPG" alt="04"/></a>
+			<a href="fotos/2016/retiro/05.JPG"> <img src="fotos/2016/retiro/05.JPG" alt="05"/></a>
+			<a href="fotos/2016/retiro/06.JPG"> <img src="fotos/2016/retiro/06.JPG" alt="06"/></a>
+			<a href="fotos/2016/retiro/07.JPG"> <img src="fotos/2016/retiro/07.JPG" alt="07"/></a>
+			<a href="fotos/2016/retiro/08.JPG"> <img src="fotos/2016/retiro/08.JPG" alt="08"/></a>
+			<a href="fotos/2016/retiro/09.JPG"> <img src="fotos/2016/retiro/09.JPG" alt="09"/></a>
+			<a href="fotos/2016/retiro/10.JPG"><img src="fotos/2016/retiro/10.JPG" alt="10"/></a>
+			<a href="fotos/2016/retiro/11.JPG"><img src="fotos/2016/retiro/11.JPG" alt="11"/></a>
+			<a href="fotos/2016/retiro/12.JPG"><img src="fotos/2016/retiro/12.JPG" alt="12"/></a>
+			<a href="fotos/2016/retiro/13.JPG"><img src="fotos/2016/retiro/13.JPG" alt="13"/></a>
+			<a href="fotos/2016/retiro/14.JPG"><img src="fotos/2016/retiro/14.JPG" alt="14"/></a>
+			<a href="fotos/2016/retiro/15.JPG"><img src="fotos/2016/retiro/15.JPG" alt="15"/></a>
+			<a href="fotos/2016/retiro/16.JPG"><img src="fotos/2016/retiro/16.JPG" alt="16"/></a>
+			<a href="fotos/2016/retiro/17.JPG"><img src="fotos/2016/retiro/17.JPG" alt="17"/></a>
+			<a href="fotos/2016/retiro/18.JPG"><img src="fotos/2016/retiro/18.JPG" alt="18"/></a>
+			<a href="fotos/2016/retiro/19.JPG"><img src="fotos/2016/retiro/19.JPG" alt="19"/></a>
+			<a href="fotos/2016/retiro/20.JPG"><img src="fotos/2016/retiro/20.JPG" alt="20"/></a>
+			<a href="fotos/2016/retiro/21.JPG"><img src="fotos/2016/retiro/21.JPG" alt="21"/></a>
+			<a href="fotos/2016/retiro/22.JPG"><img src="fotos/2016/retiro/22.JPG" alt="22"/></a>
+			<a href="fotos/2016/retiro/23.JPG"><img src="fotos/2016/retiro/23.JPG" alt="23"/></a>
+			<a href="fotos/2016/retiro/24.JPG"><img src="fotos/2016/retiro/24.JPG" alt="24"/></a>
+			<a href="fotos/2016/retiro/25.JPG"><img src="fotos/2016/retiro/25.JPG" alt="25"/></a>
+			<a href="fotos/2016/retiro/26.JPG"><img src="fotos/2016/retiro/26.JPG" alt="26"/></a>
+			<a href="fotos/2016/retiro/27.JPG"><img src="fotos/2016/retiro/27.JPG" alt="27"/></a>
+			<a href="fotos/2016/retiro/28.JPG"><img src="fotos/2016/retiro/28.JPG" alt="28"/></a>
+			<a href="fotos/2016/retiro/29.JPG"><img src="fotos/2016/retiro/29.JPG" alt="29"/></a>
+			<a href="fotos/2016/retiro/30.JPG"><img src="fotos/2016/retiro/30.JPG" alt="30"/></a>
+			<a href="fotos/2016/retiro/31.JPG"><img src="fotos/2016/retiro/31.JPG" alt="31"/></a>
+			<a href="fotos/2016/retiro/32.JPG"><img src="fotos/2016/retiro/32.JPG" alt="32"/></a>
+			<a href="fotos/2016/retiro/33.JPG"><img src="fotos/2016/retiro/33.JPG" alt="33"/></a>
+			<a href="fotos/2016/retiro/34.JPG"><img src="fotos/2016/retiro/34.JPG" alt="34"/></a>
+			<a href="fotos/2016/retiro/35.JPG"><img src="fotos/2016/retiro/35.JPG" alt="35"/></a>
+			<a href="fotos/2016/retiro/36.JPG"><img src="fotos/2016/retiro/36.JPG" alt="36"/></a>
+			<a href="fotos/2016/retiro/37.JPG"><img src="fotos/2016/retiro/37.JPG" alt="37"/></a>
+			<a href="fotos/2016/retiro/38.JPG"><img src="fotos/2016/retiro/38.JPG" alt="38"/></a>
+			<a href="fotos/2016/retiro/39.JPG"><img src="fotos/2016/retiro/39.JPG" alt="39"/></a>
+			<a href="fotos/2016/retiro/40.JPG"><img src="fotos/2016/retiro/40.JPG" alt="40"/></a>
+			<a href="fotos/2016/retiro/41.JPG"><img src="fotos/2016/retiro/41.JPG" alt="41"/></a>
+			<a href="fotos/2016/retiro/42.JPG"><img src="fotos/2016/retiro/42.JPG" alt="42"/></a>
+			<a href="fotos/2016/retiro/43.JPG"><img src="fotos/2016/retiro/43.JPG" alt="43"/></a>
+			<a href="fotos/2016/retiro/44.JPG"><img src="fotos/2016/retiro/44.JPG" alt="44"/></a>
+			<a href="fotos/2016/retiro/45.JPG"><img src="fotos/2016/retiro/45.JPG" alt="45"/></a>
+			<a href="fotos/2016/retiro/46.JPG"><img src="fotos/2016/retiro/46.JPG" alt="46"/></a>
+			<a href="fotos/2016/retiro/47.JPG"><img src="fotos/2016/retiro/47.JPG" alt="47"/></a>
+			<a href="fotos/2016/retiro/48.JPG"> <img src="fotos/2016/retiro/48.JPG" alt="48"/></a>
+			<a href="fotos/2016/retiro/49.JPG"> <img src="fotos/2016/retiro/49.JPG" alt="49"/></a>
+			<a href="fotos/2016/retiro/50.JPG"> <img src="fotos/2016/retiro/50.JPG" alt="50"/></a>
+			<a href="fotos/2016/retiro/51.JPG"> <img src="fotos/2016/retiro/51.JPG" alt="51"/></a>
+			<a href="fotos/2016/retiro/52.JPG"> <img src="fotos/2016/retiro/52.JPG" alt="52"/></a>
+			<a href="fotos/2016/retiro/53.JPG"> <img src="fotos/2016/retiro/53.JPG" alt="53"/></a>
+			<a href="fotos/2016/retiro/54.JPG"> <img src="fotos/2016/retiro/54.JPG" alt="54"/></a>
+			<a href="fotos/2016/retiro/55.JPG"> <img src="fotos/2016/retiro/55.JPG" alt="55"/></a>
+			<a href="fotos/2016/retiro/56.JPG"> <img src="fotos/2016/retiro/56.JPG" alt="56"/></a>
+			<a href="fotos/2016/retiro/57.JPG"><img src="fotos/2016/retiro/57.JPG" alt="57"/></a>
+			<a href="fotos/2016/retiro/58.JPG"><img src="fotos/2016/retiro/58.JPG" alt="58"/></a>
+			<a href="fotos/2016/retiro/59.JPG"><img src="fotos/2016/retiro/59.JPG" alt="59"/></a>
+			<a href="fotos/2016/retiro/60.JPG"><img src="fotos/2016/retiro/60.JPG" alt="60"/></a>
+			<a href="fotos/2016/retiro/61.JPG"><img src="fotos/2016/retiro/61.JPG" alt="61"/></a>
+			<a href="fotos/2016/retiro/62.JPG"><img src="fotos/2016/retiro/62.JPG" alt="62"/></a>
+			<a href="fotos/2016/retiro/63.JPG"><img src="fotos/2016/retiro/63.JPG" alt="63"/></a>
+			<a href="fotos/2016/retiro/64.JPG"><img src="fotos/2016/retiro/64.JPG" alt="64"/></a>
+			<a href="fotos/2016/retiro/65.JPG"><img src="fotos/2016/retiro/65.JPG" alt="65"/></a>
+			<a href="fotos/2016/retiro/66.JPG"><img src="fotos/2016/retiro/66.JPG" alt="66"/></a>
+			<a href="fotos/2016/retiro/67.JPG"><img src="fotos/2016/retiro/67.JPG" alt="67"/></a>
+			<a href="fotos/2016/retiro/68.JPG"><img src="fotos/2016/retiro/68.JPG" alt="68"/></a>
+			<a href="fotos/2016/retiro/69.JPG"><img src="fotos/2016/retiro/69.JPG" alt="69"/></a>
+			<a href="fotos/2016/retiro/70.JPG"><img src="fotos/2016/retiro/70.JPG" alt="70"/></a>
+			<a href="fotos/2016/retiro/71.JPG"><img src="fotos/2016/retiro/71.JPG" alt="71"/></a>
+			<a href="fotos/2016/retiro/72.JPG"><img src="fotos/2016/retiro/72.JPG" alt="72"/></a>
+			<a href="fotos/2016/retiro/73.JPG"><img src="fotos/2016/retiro/73.JPG" alt="73"/></a>
+			<a href="fotos/2016/retiro/74.JPG"><img src="fotos/2016/retiro/74.JPG" alt="74"/></a>
+			<a href="fotos/2016/retiro/75.JPG"><img src="fotos/2016/retiro/75.JPG" alt="75"/></a>
+			<a href="fotos/2016/retiro/76.JPG"><img src="fotos/2016/retiro/76.JPG" alt="76"/></a>
+			<a href="fotos/2016/retiro/77.JPG"><img src="fotos/2016/retiro/77.JPG" alt="77"/></a>
+			<a href="fotos/2016/retiro/78.JPG"><img src="fotos/2016/retiro/78.JPG" alt="78"/></a>
+			<a href="fotos/2016/retiro/79.JPG"><img src="fotos/2016/retiro/79.JPG" alt="79"/></a>
+			<a href="fotos/2016/retiro/80.JPG"><img src="fotos/2016/retiro/80.JPG" alt="80"/></a>
+			<a href="fotos/2016/retiro/81.JPG"><img src="fotos/2016/retiro/81.JPG" alt="81"/></a>
+			<a href="fotos/2016/retiro/82.JPG"><img src="fotos/2016/retiro/82.JPG" alt="82"/></a>
+			<a href="fotos/2016/retiro/83.JPG"><img src="fotos/2016/retiro/83.JPG" alt="83"/></a>
+			<a href="fotos/2016/retiro/84.JPG"><img src="fotos/2016/retiro/84.JPG" alt="84"/></a>
+			<a href="fotos/2016/retiro/85.JPG"><img src="fotos/2016/retiro/85.JPG" alt="85"/></a>
+			<a href="fotos/2016/retiro/86.JPG"><img src="fotos/2016/retiro/86.JPG" alt="86"/></a>
+			<a href="fotos/2016/retiro/87.JPG"><img src="fotos/2016/retiro/87.JPG" alt="87"/></a>
+			<a href="fotos/2016/retiro/88.JPG"><img src="fotos/2016/retiro/88.JPG" alt="88"/></a>
+			<a href="fotos/2016/retiro/89.JPG"><img src="fotos/2016/retiro/89.JPG" alt="89"/></a>
+			<a href="fotos/2016/retiro/90.JPG"><img src="fotos/2016/retiro/90.JPG" alt="90"/></a>
+			<a href="fotos/2016/retiro/91.JPG"><img src="fotos/2016/retiro/91.JPG" alt="91"/></a>
+			<a href="fotos/2016/retiro/92.JPG"><img src="fotos/2016/retiro/92.JPG" alt="92"/></a>
+			<a href="fotos/2016/retiro/93.JPG"><img src="fotos/2016/retiro/93.JPG" alt="93"/></a>
+			<a href="fotos/2016/retiro/94.JPG"><img src="fotos/2016/retiro/94.JPG" alt="94"/></a>
+			<a href="fotos/2016/retiro/95.JPG"> <img src="fotos/2016/retiro/95.JPG" alt="95"/></a>
+			<a href="fotos/2016/retiro/96.JPG"> <img src="fotos/2016/retiro/96.JPG" alt="96"/></a>
+			<a href="fotos/2016/retiro/97.JPG"> <img src="fotos/2016/retiro/97.JPG" alt="97"/></a>
+			<a href="fotos/2016/retiro/98.JPG"> <img src="fotos/2016/retiro/98.JPG" alt="98"/></a>
+			<a href="fotos/2016/retiro/99.JPG"> <img src="fotos/2016/retiro/99.JPG" alt="99"/></a>
+			<a href="fotos/2016/retiro/100.JPG"><img src="fotos/2016/retiro/100.JPG" alt="100"/></a>
+			<a href="fotos/2016/retiro/101.JPG"><img src="fotos/2016/retiro/101.JPG" alt="101"/></a>
+			<a href="fotos/2016/retiro/102.JPG"><img src="fotos/2016/retiro/102.JPG" alt="102"/></a>
+			<a href="fotos/2016/retiro/103.JPG"><img src="fotos/2016/retiro/103.JPG" alt="103"/></a>
+			<a href="fotos/2016/retiro/104.JPG"><img src="fotos/2016/retiro/104.JPG" alt="104"/></a>
+			<a href="fotos/2016/retiro/105.JPG"><img src="fotos/2016/retiro/105.JPG" alt="105"/></a>
+			<a href="fotos/2016/retiro/106.JPG"><img src="fotos/2016/retiro/106.JPG" alt="106"/></a>
+			<a href="fotos/2016/retiro/107.JPG"><img src="fotos/2016/retiro/107.JPG" alt="107"/></a>
+			<a href="fotos/2016/retiro/108.JPG"><img src="fotos/2016/retiro/108.JPG" alt="108"/></a>
+			<a href="fotos/2016/retiro/109.JPG"><img src="fotos/2016/retiro/109.JPG" alt="109"/></a>
+			<a href="fotos/2016/retiro/110.JPG"><img src="fotos/2016/retiro/110.JPG" alt="110"/></a>
+			<a href="fotos/2016/retiro/111.JPG"><img src="fotos/2016/retiro/111.JPG" alt="111"/></a>
+			<a href="fotos/2016/retiro/112.JPG"><img src="fotos/2016/retiro/112.JPG" alt="112"/></a>
+			<a href="fotos/2016/retiro/113.JPG"><img src="fotos/2016/retiro/113.JPG" alt="113"/></a>
+			<a href="fotos/2016/retiro/114.JPG"><img src="fotos/2016/retiro/114.JPG" alt="114"/></a>
+			<a href="fotos/2016/retiro/115.JPG"><img src="fotos/2016/retiro/115.JPG" alt="115"/></a>
+			<a href="fotos/2016/retiro/116.JPG"><img src="fotos/2016/retiro/116.JPG" alt="116"/></a>
+			<a href="fotos/2016/retiro/117.JPG"><img src="fotos/2016/retiro/117.JPG" alt="117"/></a>
+			<a href="fotos/2016/retiro/118.JPG"><img src="fotos/2016/retiro/118.JPG" alt="118"/></a>
+			<a href="fotos/2016/retiro/119.JPG"><img src="fotos/2016/retiro/119.JPG" alt="119"/></a>
+			<a href="fotos/2016/retiro/120.JPG"><img src="fotos/2016/retiro/120.JPG" alt="120"/></a>
+			<a href="fotos/2016/retiro/121.JPG"><img src="fotos/2016/retiro/121.JPG" alt="121"/></a>
+			<a href="fotos/2016/retiro/122.JPG"><img src="fotos/2016/retiro/122.JPG" alt="122"/></a>
+			<a href="fotos/2016/retiro/123.JPG"><img src="fotos/2016/retiro/123.JPG" alt="123"/></a>
+			<a href="fotos/2016/retiro/124.JPG"><img src="fotos/2016/retiro/124.JPG" alt="124"/></a>
+			<a href="fotos/2016/retiro/125.JPG"><img src="fotos/2016/retiro/125.JPG" alt="125"/></a>
+			<a href="fotos/2016/retiro/126.JPG"><img src="fotos/2016/retiro/126.JPG" alt="126"/></a>
+			<a href="fotos/2016/retiro/127.JPG"><img src="fotos/2016/retiro/127.JPG" alt="127"/></a>
+			<a href="fotos/2016/retiro/128.JPG"><img src="fotos/2016/retiro/128.JPG" alt="128"/></a>
+			<a href="fotos/2016/retiro/129.JPG"><img src="fotos/2016/retiro/129.JPG" alt="129"/></a>
+			<a href="fotos/2016/retiro/130.JPG"><img src="fotos/2016/retiro/130.JPG" alt="130"/></a>
+			<a href="fotos/2016/retiro/131.JPG"><img src="fotos/2016/retiro/131.JPG" alt="131"/></a>
+			<a href="fotos/2016/retiro/132.JPG"><img src="fotos/2016/retiro/132.JPG" alt="132"/></a>
+			<a href="fotos/2016/retiro/133.JPG"><img src="fotos/2016/retiro/133.JPG" alt="133"/></a>
+			<a href="fotos/2016/retiro/134.JPG"><img src="fotos/2016/retiro/134.JPG" alt="134"/></a>
+			<a href="fotos/2016/retiro/135.jpg"><img src="fotos/2016/retiro/135.jpg" alt="135"/></a>
+			<a href="fotos/2016/retiro/136.jpg"><img src="fotos/2016/retiro/136.jpg" alt="136"/></a>
+			<a href="fotos/2016/retiro/137.jpg"><img src="fotos/2016/retiro/137.jpg" alt="137"/></a>
+			<a href="fotos/2016/retiro/138.jpg"><img src="fotos/2016/retiro/138.jpg" alt="138"/></a>
+			<a href="fotos/2016/retiro/139.jpg"><img src="fotos/2016/retiro/139.jpg" alt="139"/></a>
+			<a href="fotos/2016/retiro/140.jpg"><img src="fotos/2016/retiro/140.jpg" alt="140"/></a>
+			<a href="fotos/2016/retiro/141.jpg"><img src="fotos/2016/retiro/141.jpg" alt="141"/></a>
+			<a href="fotos/2016/retiro/142.jpg"><img src="fotos/2016/retiro/142.jpg" alt="142"/></a>
+			<a href="fotos/2016/retiro/143.jpg"><img src="fotos/2016/retiro/143.jpg" alt="143"/></a>
+			<a href="fotos/2016/retiro/144.jpg"><img src="fotos/2016/retiro/144.jpg" alt="144"/></a>
+			<a href="fotos/2016/retiro/145.jpg"><img src="fotos/2016/retiro/145.jpg" alt="145"/></a>
+			<a href="fotos/2016/retiro/146.jpg"><img src="fotos/2016/retiro/146.jpg" alt="146"/></a>
+			<a href="fotos/2016/retiro/147.jpg"><img src="fotos/2016/retiro/147.jpg" alt="147"/></a>
+			<a href="fotos/2016/retiro/148.jpg"><img src="fotos/2016/retiro/148.jpg" alt="148"/></a>
+			<a href="fotos/2016/retiro/149.jpg"><img src="fotos/2016/retiro/149.jpg" alt="149"/></a>
+			<a href="fotos/2016/retiro/150.jpg"><img src="fotos/2016/retiro/150.jpg" alt="150"/></a>
+			<a href="fotos/2016/retiro/151.jpg"><img src="fotos/2016/retiro/151.jpg" alt="151"/></a>
+			<a href="fotos/2016/retiro/152.jpg"><img src="fotos/2016/retiro/152.jpg" alt="152"/></a>
+			<a href="fotos/2016/retiro/153.jpg"><img src="fotos/2016/retiro/153.jpg" alt="153"/></a>
+			<a href="fotos/2016/retiro/154.jpg"><img src="fotos/2016/retiro/154.jpg" alt="154"/></a>
+			<a href="fotos/2016/retiro/155.jpg"><img src="fotos/2016/retiro/155.jpg" alt="155"/></a>
+			<a href="fotos/2016/retiro/156.jpg"><img src="fotos/2016/retiro/156.jpg" alt="156"/></a>
+			<a href="fotos/2016/retiro/157.jpg"><img src="fotos/2016/retiro/157.jpg" alt="157"/></a>
+			<a href="fotos/2016/retiro/158.jpg"><img src="fotos/2016/retiro/158.jpg" alt="158"/></a>
+			<a href="fotos/2016/retiro/159.jpg"><img src="fotos/2016/retiro/159.jpg" alt="159"/></a>
+			<a href="fotos/2016/retiro/160.jpg"><img src="fotos/2016/retiro/160.jpg" alt="160"/></a>
+			<a href="fotos/2016/retiro/161.jpg"><img src="fotos/2016/retiro/161.jpg" alt="161"/></a>
+			<a href="fotos/2016/retiro/162.jpg"><img src="fotos/2016/retiro/162.jpg" alt="162"/></a>
+			<a href="fotos/2016/retiro/163.jpg"><img src="fotos/2016/retiro/163.jpg" alt="163"/></a>
+			<a href="fotos/2016/retiro/164.jpg"><img src="fotos/2016/retiro/164.jpg" alt="164"/></a>
+			<a href="fotos/2016/retiro/165.jpg"><img src="fotos/2016/retiro/165.jpg" alt="165"/></a>
+			<a href="fotos/2016/retiro/166.jpg"><img src="fotos/2016/retiro/166.jpg" alt="166"/></a>
+			<a href="fotos/2016/retiro/167.jpg"><img src="fotos/2016/retiro/167.jpg" alt="167"/></a>
+			<a href="fotos/2016/retiro/168.jpg"><img src="fotos/2016/retiro/168.jpg" alt="168"/></a>
+			<a href="fotos/2016/retiro/169.jpg"><img src="fotos/2016/retiro/169.jpg" alt="169"/></a>
+			<a href="fotos/2016/retiro/170.jpg"><img src="fotos/2016/retiro/170.jpg" alt="170"/></a>
+			<a href="fotos/2016/retiro/171.jpg"><img src="fotos/2016/retiro/171.jpg" alt="171"/></a>
+			<a href="fotos/2016/retiro/172.jpg"><img src="fotos/2016/retiro/172.jpg" alt="172"/></a>
+			<a href="fotos/2016/retiro/173.jpg"><img src="fotos/2016/retiro/173.jpg" alt="173"/></a>
+			<a href="fotos/2016/retiro/174.jpg"><img src="fotos/2016/retiro/174.jpg" alt="174"/></a>
+			<a href="fotos/2016/retiro/175.jpg"><img src="fotos/2016/retiro/175.jpg" alt="175"/></a>
+			<a href="fotos/2016/retiro/176.jpg"><img src="fotos/2016/retiro/176.jpg" alt="176"/></a>
+			<a href="fotos/2016/retiro/177.jpg"><img src="fotos/2016/retiro/177.jpg" alt="177"/></a>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html';?>
