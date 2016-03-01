6253f05edf11315a9cc5ac2067f27c03812417e6
BrunoDM2943 <bdm2943@gmail.com>
2016-02-29 21:03:51 -0300

    Atualização de eventos e aniversariantes

--- eventos.php
+++ eventos.php
@@ -5,11 +5,22 @@
 	</div>
 	<div class="panel-body">
 		<div class="list-group">
-			<br> <a href="#" class="list-group-item-active">
-			<h4 class="list-group-item-heading">Reunião do Calendário</h4>
+			<br>
+			<h4 class="list-group-item-heading">Louvor em Suzano</h4>
+			<p class="list-group-item-text">Local: IEP Suzano</p>
+			<p class="list-group-item-text">Endereço:Av. Gov. Mario Covas, 889 - Vila Nova Urupes, Suzano</p>
+			<p class="list-group-item-text">Data: Sábado 05/03 - 15h00</p>
+			<div  class="embed-responsive embed-responsive-16by9" id="map">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1828.8487240609868!2d-46.30477140001906!3d-23.543381909272515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x633ad91d76595df!2sIgreja+Evang%C3%A9lica+de+Pinheiros+em+Suzano!5e0!3m2!1spt-BR!2sbr!4v1456789776041" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		<br>
+		</div>
+		<div class="list-group">
+			<br>
+			<h4 class="list-group-item-heading">Reunião de professores</h4>
 			<p class="list-group-item-text">Local: IEPEM</p>
-			<p class="list-group-item-text">Data: Sábado 13/02 - 18h00</p>
-		</a> <br>
+			<p class="list-group-item-text">Data: Sábado 12/03 - 10h00</p>
+			<br>
 		</div>
 	</div>
 </div>
