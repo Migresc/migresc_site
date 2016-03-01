87dd84eeebb46a253426d396177ea9e08e492ecf
BrunoDM2943 <bdm2943@gmail.com>
2016-02-29 21:07:02 -0300

    Home

--- index.php
+++ index.php
@@ -4,8 +4,7 @@
         <h3 class="panel-title">Seja bem vindo!</h3>
     </div>
     <div class="panel-body">
-        <h3>Retire aqui a ficha de inscrição para o nosso Retiro 2016!
-        <a href="./arquivos/fichaRetiro2016.pdf" class="glyphicon glyphicon-download"> </a></h3>
+        <h3>JÃ¡ deu uma olhada nas <a href="./fotos-retiro.php"> fotos</a> do retiro?</h3>
     </div>
 </div>
 <?php include 'footer.html';?>
