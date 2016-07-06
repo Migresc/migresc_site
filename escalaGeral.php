7e173d9532a50c8c38fd4d72ada8cde242d7ced9
BrunoDM2943 <bdm2943@gmail.com>
2016-07-05 22:33:58 -0300

    Criação da página de escala de funções

---
+++ escalaGeral.php
@@ -0,0 +1,184 @@
+<?php include 'header.html';?>
+<div class="panel panel-inf">
+    <!-- Default panel contents -->
+    <div class="panel-heading">Julho/2016 - Manhã</div>
+
+    <div class="table-responsive">
+    <!-- Table -->
+        <table class="table">
+            <tr>
+                <th>DATA</th>
+                <th>PORTAS</th>
+                <th>RECEPÇÃO</th>
+                <th>LOUVOR INFANTIL EBD</th>
+                <th>VERSÍCULO DO MÊS - CLASSE</th>
+                <th>ABERTURA EBD</th>
+                <th>LOUSA/PINCEL ETC</th>
+                <th>LANCHE JD. LAPENA</th>
+                <th>MONITOR JD. MARILÚ</th>
+                <th>COZINHA</th>
+                <th>LANCHE SERVIDO</th>
+            </tr>
+            <tr>
+                <td>03/07/2016</td>
+                <td>Rebeca Hilary</td>
+                <td>Dulcinéia</td>
+                <td>Gabriela Al., Danielle e Guilherme</td>
+                <td>Pré-Adolescentes</td>
+                <td>Manassés</td>
+                <td>Kailaine</td>
+                <td>Francisco</td>
+                <td>Felipe B.</td>
+                <td>José Antônio / Cleonice e Cristian</td>
+                <td>Mortadela</td>
+            </tr>
+            <tr>
+                <td>10/07/2016</td>
+                <td>Matheus Américo</td>
+                <td>Lídia</td>
+                <td>Luiza, Danielle e Celso</td>
+                <td>Pré-Adolescentes</td>
+                <td>Emerson</td>
+                <td>Yasmin</td>
+                <td>Altemir</td>
+                <td>Fernando</td>
+                <td>Neusa / Eva e Rose</td>
+                <td>Salsicha</td>
+            </tr>
+            <tr>
+                <td>17/07/2016</td>
+                <td>Rebeca Hilary</td>
+                <td>Felipe Al.</td>
+                <td>Gabriela Al., Danielle e Guilherme</td>
+                <td>Pré-Adolescentes</td>
+                <td>Luciano</td>
+                <td>Victória Martha</td>
+                <td>Débora Crist.</td>
+                <td>José Antônio</td>
+                <td>Rosana / Cleonice e Angelina</td>
+                <td>Bolo</td>
+            </tr>
+            <tr>
+                <td>24/07/2016</td>
+                <td>Matheus Américo</td>
+                <td>Altemir</td>
+                <td>Luiza, Danielle e Celso</td>
+                <td>Pré-Adolescentes</td>
+                <td>Moisés</td>
+                <td>Kailaine</td>
+                <td>Gabriela Al.</td>
+                <td>Guilherme</td>
+                <td>José Antônio / Luiza e Ana Luiza</td>
+                <td>Pão com Requeijão</td>
+            </tr>
+            <tr>
+                <td>31/07/2016</td>
+                <td>Rebeca Hilary</td>
+                <td>Rafaela</td>
+                <td>Gabriela Al., Danielle e Guilherme</td>
+                <td>Pré-Adolescentes</td>
+                <td>Manassés</td>
+                <td>Yasmin</td>
+                <td>Rodrigo</td>
+                <td>Altemir</td>
+                <td>Neusa / Ednéia e Eva</td>
+                <td>Frango</td>
+            </tr>
+        </table>
+    </div>
+</div>
+</p>
+<div class="panel panel-inf">
+    <div class="panel-heading">Julho/2016 - Noite</div>
+
+    <div class="table-responsive">
+        <!-- Table -->
+        <table class="table">
+            <tr>
+                <th>DATA</th>
+                <th>PORTARIA</th>
+                <th>RECEPÇÃO</th>
+                <th>MICROFONE VOLANTE</th>
+                <th>PORTAS e JANELAS</th>
+                <th>CORTINAS</th>
+                <th>SOM </th>
+                <th>DATA-SHOW</th>
+                <th>FILMAGEM</th>
+                <th>FOTOGRAFIA</th>
+                <th>GRUPO DE LOUVOR</th>
+                <th>RELATÓRIO DOS CULTOS</th>
+            </tr>
+            <tr>
+                <td>03/07/2016</td>
+                <td>Gilvan</td>
+                <td>Clarice e Kailaine</td>
+                <td>Gabriela Bezerra</td>
+                <td>-----------</td>
+                <td>Luiz Fernando</td>
+                <td>Felipe B. e Éder</td>
+                <td>Lídia</td>
+                <td>Janderson</td>
+                <td>Amanda Fern</td>
+                <td>Pastores</td>
+                <td>Emerson</td>
+            </tr>
+            <tr>
+                <td>10/07/2016</td>
+                <td>Luciano</td>
+                <td>Lídia e Danielle Parra</td>
+                <td>Kailaine</td>
+                <td>Luiz Fernando</td>
+                <td>Isabella</td>
+                <td>Éder e Rafael</td>
+                <td>Beatriz</td>
+                <td>José Antônio</td>
+                <td>Victória Martha</td>
+                <td>Grupo Ágape</td>
+                <td>Janderson</td>
+            </tr>
+            <tr>
+                <td>17/07/2016</td>
+                <td>Francisco</td>
+                <td>Ana Luiza e Beth</td>
+                <td>Letícia</td>
+                <td>Lídia</td>
+                <td>Vinícius</td>
+                <td>Rafael e Janderson</td>
+                <td>Gabriela Almeida</td>
+                <td>Felipe Paiva</td>
+                <td>Lídia</td>
+                <td>Jovens</td>
+                <td>Israel</td>
+            </tr>
+            <tr>
+                <td>24/07/2016</td>
+                <td>José Antônio</td>
+                <td>Amanda e Eva</td>
+                <td>Matheus Souza</td>
+                <td>Danielle Parra</td>
+                <td>Victória  Martha</td>
+                <td>Janderson e Bruno</td>
+                <td>Matheus Américo</td>
+                <td>Lídia</td>
+                <td>Isabella</td>
+                <td>Casados</td>
+                <td>Luciano</td>
+            </tr>
+            <tr>
+                <td>31/07/2016</td>
+                <td>Flávio</td>
+                <td>Claudete e Angelina</td>
+                <td>Isabella</td>
+                <td>Luiz Fernando</td>
+                <td>Gustavo</td>
+                <td>Bruno e Felipe B.</td>
+                <td>Letícia</td>
+                <td>José Antônio</td>
+                <td>Victória Almeida</td>
+                <td>Pastores</td>
+                <td>Emerson</td>
+            </tr>
+        </table>
+    </div>
+</div>
+<?php include 'footer.html';?>
