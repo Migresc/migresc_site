d337ab15d5b7ad91ec1fdba8343b86ad15aae171
BrunoDM2943 <bdm2943@gmail.com>
2016-08-23 20:54:55 -0300

    Escala de funções para o aniversário da igreja

---
+++ escalaAniversarioIEP.php
@@ -0,0 +1,44 @@
+<?php include 'header.html';?>
+<div class="panel panel-info">
+    <!-- Default panel contents -->
+    <div class="panel-heading">
+        <h3 class="panel-title">Agosto/2016 - Sábado à noite - 27 de Agosto de 2016</h3>
+    </div>
+    <div class="panel-body">
+        <div class="table-responsive">
+        <!-- Table -->
+            <table class="table">
+                <thead>
+                <th>DATA</th>
+                <th>PORTARIA</th>
+                <th>RECEPÇÃO</th>
+                <th>PORTAS e JANELAS</th>
+                <th>CORTINAS</th>
+                <th>SOM </th>
+                <th>DATA-SHOW</th>
+                <th>FILMAGEM</th>
+                <th>FOTOGRAFIA</th>
+                <th>GRUPO DE LOUVOR</th>
+                <th>DIREÇÃO DO CULTO</th>
+                <th>EQUIPE COZINHA</th>
+                </thead>
+                <tr>
+                    <td>27/08/2016</td>
+                    <td>Moises e demais porteiros/seguranças</td>
+                    <td>Claudete, Eva, Beth, Clarice</td>
+                    <td>Luiz Fernando e Danielle Parra</td>
+                    <td>Gustavo  Vinícius</td>
+                    <td>Bruno, Éder e Felipe B.</td>
+                    <td>Bia</td>
+                    <td>Janderson</td>
+                    <td>Victória Almeida e Janderson</td>
+                    <td>Responsável Luiza em nossa IEP</td>
+                    <td>Pr. Claudio</td>
+                    <td>Ana Galvão. Manaself, Cristian</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
+</p>
+<?php include 'footer.html';?>
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <h3 class="panel-title">Seja bem vindo!</h3>
     </div>
     <div class="panel-body">
-        <h3>Já deu uma olhada na <a href="./escalaGeral.php"> escala de funções</a> do mês de Agosto?</h3>
+        <h3><a href="./escalaAniversarioIEP.php">Veja aqui a escala de funções para o dia 27 de Agosto!!</a></h3>
     </div>
 </div>
 <?php include 'footer.html';?>
