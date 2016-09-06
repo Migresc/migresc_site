864f89298e84d7279ba5ba5fc8b62d55e5bff5fa
BrunoDM2943 <bdm2943@gmail.com>
2016-09-06 06:50:34 -0300

    Notícias

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 ﻿<?php include 'header.html';?>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Seja bem vindo!</h3>
+        <h3 class="panel-title">Nota de falecimento</h3>
     </div>
     <div class="panel-body">
-        <h3><a href="./escalaAniversarioIEP.php">Veja aqui a escala de funções para o dia 27 de Agosto!!</a></h3>
+        <h3 >
+        É com grande pesar que informamos que o nosso irmão Antonio Durante faleceu ontem
+        às 16 horas. O velório acontecerá no Cemitério da Quarta Parada, Av.Salim Farah Maluf, s/n - Água Rasa.
+        O sepultamento ocorrerá no mesmo local, às 16 horas.
+        </h3>
     </div>
 </div>
 <?php include 'footer.html';?>
