0d9e83b630456d1e778a63901e305354f73ee78b
BrunoDM2943 <bdm2943@gmail.com>
2016-09-13 21:32:02 -0300

    Atualização

--- escalaGeral.php
+++ escalaGeral.php
@@ -2,7 +2,7 @@
 <div class="panel panel-info">
     <!-- Default panel contents -->
     <div class="panel-heading">
-        <h3 class="panel-title">Agosto/2016 - Manhã</h3>
+        <h3 class="panel-title">Setembro/2016 - Manhã</h3>
     </div>
     <div class="panel-body">
         <div class="table-responsive">
@@ -22,55 +22,55 @@
                 <th>LANCHE SERVIDO</th>
                 </thead>
                 <tr>
-                    <td>07/08/2016</td>
+                    <td>04/09/2016</td>
                     <td>Matheus Américo</td>
-                    <td>Felipe Almeida</td>
-                    <td>Luiza, Danielle e Celso</td>
-                    <td>Jovens</td>
+                    <td>Altemir</td>
+                    <td>Luiza, Danielle, Celso</td>
+                    <td>Cordeirinhos de Jesus</td>
                     <td>Moisés</td>
-                    <td>Yasmin</td>
-                    <td>Celso</td>
+                    <td>Victória Martha</td>
+                    <td>Francisco</td>
+                    <td>José Antônio</td>
+                    <td>Cleonice, Ana Luiza, Lindália</td>
+                    <td>Pão com Requeijão</td>
+                </tr>
+                <tr>
+                    <td>11/09/2016</td>
+                    <td>Rebeca Hilary</td>
+                    <td>Dulcinéia</td>
+                    <td>Gabriela Al., Danielle, Guilherme</td>
+                    <td>Cordeirinhos de Jesus</td>
+                    <td>Emerson</td>
+                    <td>Kailaine</td>
+                    <td>Rodrigo</td>
                     <td>Felipe Paiva</td>
-                    <td>Lindália, Cleonice e Ana Luiza</td>
+                    <td>Angelina, José Antônio, Eva</td>
                     <td>Mortadela</td>
                 </tr>
                 <tr>
-                    <td>14/08/2016</td>
-                    <td>Rebeca Hilary</td>
+                    <td>18/09/2016</td>
+                    <td>Matheus Américo</td>
+                    <td>Rafaela</td>
+                    <td>Luiza, Danielle, Celso</td>
+                    <td>Cordeirinhos de Jesus</td>
+                    <td>Luciano</td>
+                    <td>Yasmin</td>
                     <td>Altemir</td>
-                    <td>Gabriela Al., Daniele e Guilherme</td>
-                    <td>Jovens</td>
-                    <td>Emerson</td>
-                    <td>Kailaine</td>
-                    <td>Débora Crist.</td>
-                    <td>José Antônio</td>
-                    <td>Neuza, Eva e Rose</td>
+                    <td>Felipe Bernardo</td>
+                    <td>Lindália, Neuza, Rosana</td>
                     <td>Salsicha</td>
                 </tr>
                 <tr>
-                    <td>21/08/2016</td>
-                    <td>Matheus Américo</td>
-                    <td>Rafaela</td>
-                    <td>Luiza, Danielle e Celso</td>
-                    <td>Jovens</td>
-                    <td>Luciano</td>
-                    <td>Victória Martha</td>
-                    <td>Francisco</td>
-                    <td>Felipe B.</td>
-                    <td>Angelina, Ednéia e Eva</td>
-                    <td>Pão com queijo ou Manteiga</td>
-                </tr>
-                <tr>
-                    <td>28/08/2016</td>
+                    <td>25/09/2016</td>
                     <td>Rebeca Hilary</td>
-                    <td>Dulcinéia</td>
-                    <td>Gabriela Al., Daniele e Guilherme</td>
-                    <td>Jovens</td>
+                    <td>Felipe Al.</td>
+                    <td>Gabriela Al., Danielle, Guilherme</td>
+                    <td>Cordeirinhos de Jesus</td>
                     <td>Manassés</td>
-                    <td>Yasmin</td>
-                    <td>Altemir</td>
+                    <td>Victória Martha</td>
+                    <td>Celso</td>
                     <td>Fernando</td>
-                    <td>Cristian, Cleonice e José Antônio</td>
+                    <td>Cleonice, Eva, Rosemeire</td>
                     <td>Frango</td>
                 </tr>
             </table>
@@ -80,7 +80,7 @@
 </p>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Agosto/2016 - Noite</h3>
+        <h3 class="panel-title">Setembro/2016 - Noite</h3>
     </div>
 
     <div class="panel-body">
@@ -99,58 +99,63 @@
                 <th>FILMAGEM</th>
                 <th>FOTOGRAFIA</th>
                 <th>GRUPO DE LOUVOR</th>
+                <th>RELATÓRIO DOS CULTOS</th>
                 </thead>
                 <tr>
-                    <td>07/08/2016</td>
-                    <td>Luciano</td>
-                    <td>Cleonice e Victória Martha</td>
-                    <td>Gabriela Bezerra</td>
-                    <td>Danielie Parra</td>
-                    <td>Isabella</td>
-                    <td>Felipe B. e Éder</td>
-                    <td>Lídia</td>
-                    <td>Janderson</td>
-                    <td>Victória Almeida</td>
-                    <td>Grupo Ágape</td>
-                </tr>
-                <tr>
-                    <td>14/08/2016</td>
-                    <td>Glaylton</td>
-                    <td>Danielle Parra e Eva</td>
-                    <td>Kailaine</td>
+                    <td>04/09/2016</td>
+                    <td>Fernando</td>
+                    <td>Kailaine e Clarice</td>
+                    <td>Matheus Souza</td>
                     <td>Luiz Fernando</td>
-                    <td>Vinícius</td>
-                    <td>Éder e Rafael</td>
-                    <td>Beatriz</td>
-                    <td>Lídia</td>
-                    <td>Victória Martha</td>
-                    <td>Jovens</td>
-                </tr>
-                <tr>
-                    <td>21/08/2016</td>
-                    <td>Gilvan</td>
-                    <td>Kailaine e Lourdes</td>
+                    <td>Gustavo</td>
+                    <td>Bruno e Felipe B.</td>
                     <td>Letícia</td>
                     <td>Lídia</td>
-                    <td>Yasmin</td>
-                    <td>Rafael e Janderson</td>
-                    <td>Gabriela Almeida</td>
-                    <td>Felipe Paiva</td>
-                    <td>Isabella</td>
-                    <td>Casados</td>
+                    <td>Victória Almeida</td>
+                    <td>Grupo Ágape</td>
+                    <td>Israel</td>
                 </tr>
                 <tr>
-                    <td>28/08/2016</td>
-                    <td>Flávio</td>
-                    <td>Letícia e Ana Luiza</td>
+                    <td>11/09/2016</td>
+                    <td>Francisco</td>
+                    <td>Lourdes e Ana Luiza</td>
+                    <td>Isabella</td>
+                    <td>Lídia</td>
+                    <td>Yasmin</td>
+                    <td>Felipe B e Éder</td>
+                    <td>Beatriz</td>
+                    <td>José Antônio</td>
+                    <td>Victória Martha</td>
+                    <td>Jovens</td>
+                    <td>Janderson</td>
+                </tr>
+                <tr>
+                    <td>18/09/2016</td>
+                    <td>Moisés</td>
+                    <td>Beth e Angelina</td>
                     <td>Victória Martha</td>
                     <td>Danielle Parra</td>
-                    <td>Gustavo</td>
-                    <td>Janderson e Bruno</td>
-                    <td>Matheus Américo</td>
+                    <td>Luiz Fernando</td>
+                    <td>Éder e Rafael</td>
+                    <td>Lídia</td>
+                    <td>Janderson</td>
+                    <td>Isabella</td>
+                    <td>Casados</td>
+                    <td>Luciano</td>
+                </tr>
+                <tr>
+                    <td>25/09/2016</td>
                     <td>José Antônio</td>
+                    <td>Victória Martha e Letícia </td>
+                    <td>Gabriela Bezerra</td>
+                    <td>Luiz Fernando</td>
+                    <td>Kailaine</td>
+                    <td>Rafael e Janderson</td>
+                    <td>Gabriela Al.</td>
+                    <td>Felipe Paiva</td>
                     <td>Lídia</td>
                     <td>Pastores</td>
+                    <td>Israel</td>
                 </tr>
             </table>
         </div>
--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 ﻿<?php include 'header.html';?>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Nota de falecimento</h3>
+        <h3 class="panel-title">Pedido de Oração</h3>
     </div>
     <div class="panel-body">
         <h3 >
-        É com grande pesar que informamos que o nosso irmão Antonio Durante faleceu ontem
-        às 16 horas. O velório acontecerá no Cemitério da Quarta Parada, Av.Salim Farah Maluf, s/n - Água Rasa.
-        O sepultamento ocorrerá no mesmo local, às 16 horas.
+        A nossa irmã Ednea está internada com o quadro de dores na cabeça, abdómen e nas pernas. A pressão sanguínea
+            esta variando entre 9-10 por 6, com 50 batimentos por minuto. Os médicos ainda não encontraram o problema, por isso
+            ela está na UTI como medida preventiva. Vamos orar por ela!
+
         </h3>
     </div>
 </div>
