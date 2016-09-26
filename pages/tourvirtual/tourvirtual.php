917c38cb73efb70185e495232a2a128acd5fb4bf
BrunoDM2943 <bdm2943@gmail.com>
2016-09-26 17:29:54 -0300

    Novas Paginas

---
+++ pages/tourvirtual/tourvirtual.php
@@ -0,0 +1,26 @@
+<?php include 'header.html';?>
+<script type="text/javascript">
+$( function() {
+    $( '#gallery' ).jGallery();
+} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Tour Virtual</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+			<a href="./Banheiro.JPG"><img src="./Banheiro.JPG" alt="Banheiro"/></a>
+      <a href="./Bercario.JPG"><img src="./Bercario.JPG" alt="Berçário"/></a>
+      <a href="./Cozinha.JPG"><img src="./Cozinha.JPG" alt="Cozinha"/></a>
+      <a href="./Espaco_Gourmet.JPG"><img src="./Espaco_Gourmet.JPG" alt="Espaco Gourmet"/></a>
+      <a href="./Fachada.JPG"><img src="./Fachada.JPG" alt="Fachada"/></a>
+      <a href="./Piano.JPG"><img src="./Piano.JPG" alt="Piano"/></a>
+      <a href="./Sala_de_reuniao.JPG"><img src="./Sala_de_reuniao.JPG" alt="Sala de reunião"/></a>
+      <a href="./Sala_EBD.JPG"><img src="./Sala_EBD.JPG" alt="Sala EBD"/></a>
+      <a href="./Salao_inferior.JPG"><img src="./Salao_inferior.JPG" alt="Salão inferior"/></a>
+      <a href="./Secretaria.JPG"><img src="./Secretaria.JPG" alt="Secretaria"/></a>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html';?>
