242f59a6fa974ee6fe6a5a778a61c039747be604
BrunoDM2943 <bdm2943@gmail.com>
2016-10-09 16:15:03 -0300

    Atualização da escala
    Outubro/2016

--- escalaGeral.php
+++ escalaGeral.php
@@ -2,161 +2,188 @@
 <div class="panel panel-info">
     <!-- Default panel contents -->
     <div class="panel-heading">
-        <h3 class="panel-title">Setembro/2016 - Manhã</h3>
+        <h3 class="panel-title">Outubro/2016 - Manhã</h3>
     </div>
     <div class="panel-body">
         <div class="table-responsive">
         <!-- Table -->
-            <table class="table">
-                <thead>
-                <th>DATA</th>
-                <th>PORTAS</th>
-                <th>RECEPÇÃO</th>
-                <th>LOUVOR INFANTIL EBD</th>
-                <th>VERSÍCULO DO MÊS - CLASSE</th>
-                <th>ABERTURA EBD</th>
-                <th>LOUSA/PINCEL ETC</th>
-                <th>LANCHE JD. LAPENA</th>
-                <th>MONITOR JD. MARILÚ</th>
-                <th>COZINHA</th>
-                <th>LANCHE SERVIDO</th>
-                </thead>
-                <tr>
-                    <td>04/09/2016</td>
-                    <td>Matheus Américo</td>
-                    <td>Altemir</td>
-                    <td>Luiza, Danielle, Celso</td>
-                    <td>Cordeirinhos de Jesus</td>
-                    <td>Moisés</td>
-                    <td>Victória Martha</td>
-                    <td>Francisco</td>
-                    <td>José Antônio</td>
-                    <td>Cleonice, Ana Luiza, Lindália</td>
-                    <td>Pão com Requeijão</td>
-                </tr>
-                <tr>
-                    <td>11/09/2016</td>
-                    <td>Rebeca Hilary</td>
-                    <td>Dulcinéia</td>
-                    <td>Gabriela Al., Danielle, Guilherme</td>
-                    <td>Cordeirinhos de Jesus</td>
-                    <td>Emerson</td>
-                    <td>Kailaine</td>
-                    <td>Rodrigo</td>
-                    <td>Felipe Paiva</td>
-                    <td>Angelina, José Antônio, Eva</td>
-                    <td>Mortadela</td>
-                </tr>
-                <tr>
-                    <td>18/09/2016</td>
-                    <td>Matheus Américo</td>
-                    <td>Rafaela</td>
-                    <td>Luiza, Danielle, Celso</td>
-                    <td>Cordeirinhos de Jesus</td>
-                    <td>Luciano</td>
-                    <td>Yasmin</td>
-                    <td>Altemir</td>
-                    <td>Felipe Bernardo</td>
-                    <td>Lindália, Neuza, Rosana</td>
-                    <td>Salsicha</td>
-                </tr>
-                <tr>
-                    <td>25/09/2016</td>
-                    <td>Rebeca Hilary</td>
-                    <td>Felipe Al.</td>
-                    <td>Gabriela Al., Danielle, Guilherme</td>
-                    <td>Cordeirinhos de Jesus</td>
-                    <td>Manassés</td>
-                    <td>Victória Martha</td>
-                    <td>Celso</td>
-                    <td>Fernando</td>
-                    <td>Cleonice, Eva, Rosemeire</td>
-                    <td>Frango</td>
-                </tr>
-            </table>
+        <table class="table">
+          <thead>
+           <th>DATA</th>
+           <th>PORTAS</th>
+           <th>RECEPÇÃO</th>
+           <th>LOUVOR INFANTIL EBD</th>
+           <th>VERSÍCULO DO MÊS - CLASSE</th>
+           <th>ABERTURA EBD</th>
+           <th>LOUSA/PINCEL ETC</th>
+           <th>LANCHE JD. LAPENA</th>
+           <th>MONITOR JD. MARILÚ</th>
+           <th>COZINHA</th>
+           <th>LANCHE SERVIDO</th>
+          </thead>
+          <tr>
+           <td>02/10/2016</td>
+           <td>Matheus Américo</td>
+           <td>Lídia</td>
+           <td>Luiza, Danielle, Celso</td>
+           <td>Videiras</td>
+           <td>Moisés</td>
+           <td>Kailaine</td>
+           <td>Altemir</td>
+           <td>Felipe Paiva</td>
+           <td>Lindália, Eva, Cleonice</td>
+           <td>Salsicha</td>
+          </tr>
+          <tr>
+           <td>09/10/2016</td>
+           <td>Rebeca Hilary</td>
+           <td>Dulcinéia</td>
+           <td>Gabriela Al., Danielle, Guilherme</td>
+           <td>Videiras</td>
+           <td>Emerson</td>
+           <td>Yasmin</td>
+           <td>Francisco</td>
+           <td>José Antônio</td>
+           <td>Luíza, Cristian, Ana Luisa</td>
+           <td>Mortadela</td>
+          </tr>
+          <tr>
+           <td>16/10/2016</td>
+           <td>Matheus Américo</td>
+           <td>Altemir</td>
+           <td>Luiza, Danielle, Celso</td>
+           <td>Videiras</td>
+           <td>Luciano</td>
+           <td>Victória Martha</td>
+           <td>Débora Cristina</td>
+           <td>Emerson</td>
+           <td>Eva, José Antônio, Neuza</td>
+           <td>Pão com Requeijão</td>
+          </tr>
+          <tr>
+           <td>23/10/2016</td>
+           <td>Rebeca Hilary</td>
+           <td>Felipe Al.</td>
+           <td>Gabriela Al., Danielle, Guilherme</td>
+           <td>Videiras</td>
+           <td>Manassés</td>
+           <td>Kailaine</td>
+           <td>Felipe Al.</td>
+           <td>Fernando</td>
+           <td>José Antônio, Neuza, Angelina</td>
+           <td>Carne Vermelha</td>
+          </tr>
+          <tr>
+           <td>30/10/2016</td>
+           <td>Matheus Américo</td>
+           <td>Rafaela</td>
+           <td>Luiza, Danielle, Celso</td>
+           <td>Videiras</td>
+           <td>Luciano</td>
+           <td>Yasmin</td>
+           <td>Rodrigo</td>
+           <td>Felipe B.</td>
+           <td>Cleonice, Lindália e Rose</td>
+           <td>Bolo</td>
+          </tr>
+        </table>
         </div>
     </div>
 </div>
 </p>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Setembro/2016 - Noite</h3>
+        <h3 class="panel-title">Outubro/2016 - Noite</h3>
     </div>
 
     <div class="panel-body">
         <div class="table-responsive">
             <!-- Table -->
             <table class="table">
-                <thead>
-                <th>DATA</th>
-                <th>PORTARIA</th>
-                <th>RECEPÇÃO</th>
-                <th>MICROFONE VOLANTE</th>
-                <th>PORTAS e JANELAS</th>
-                <th>CORTINAS</th>
-                <th>SOM </th>
-                <th>DATA-SHOW</th>
-                <th>FILMAGEM</th>
-                <th>FOTOGRAFIA</th>
-                <th>GRUPO DE LOUVOR</th>
-                <th>RELATÓRIO DOS CULTOS</th>
-                </thead>
-                <tr>
-                    <td>04/09/2016</td>
-                    <td>Fernando</td>
-                    <td>Kailaine e Clarice</td>
-                    <td>Matheus Souza</td>
-                    <td>Luiz Fernando</td>
-                    <td>Gustavo</td>
-                    <td>Bruno e Felipe B.</td>
-                    <td>Letícia</td>
-                    <td>Lídia</td>
-                    <td>Victória Almeida</td>
-                    <td>Grupo Ágape</td>
-                    <td>Israel</td>
-                </tr>
-                <tr>
-                    <td>11/09/2016</td>
-                    <td>Francisco</td>
-                    <td>Lourdes e Ana Luiza</td>
-                    <td>Isabella</td>
-                    <td>Lídia</td>
-                    <td>Yasmin</td>
-                    <td>Felipe B e Éder</td>
-                    <td>Beatriz</td>
-                    <td>José Antônio</td>
-                    <td>Victória Martha</td>
-                    <td>Jovens</td>
-                    <td>Janderson</td>
-                </tr>
-                <tr>
-                    <td>18/09/2016</td>
-                    <td>Moisés</td>
-                    <td>Beth e Angelina</td>
-                    <td>Victória Martha</td>
-                    <td>Danielle Parra</td>
-                    <td>Luiz Fernando</td>
-                    <td>Éder e Rafael</td>
-                    <td>Lídia</td>
-                    <td>Janderson</td>
-                    <td>Isabella</td>
-                    <td>Casados</td>
-                    <td>Luciano</td>
-                </tr>
-                <tr>
-                    <td>25/09/2016</td>
-                    <td>José Antônio</td>
-                    <td>Victória Martha e Letícia </td>
-                    <td>Gabriela Bezerra</td>
-                    <td>Luiz Fernando</td>
-                    <td>Kailaine</td>
-                    <td>Rafael e Janderson</td>
-                    <td>Gabriela Al.</td>
-                    <td>Felipe Paiva</td>
-                    <td>Lídia</td>
-                    <td>Pastores</td>
-                    <td>Israel</td>
-                </tr>
+            	 <thead>
+             	 	 <th>DATA</th>
+             	 	 <th>PORTARIA</th>
+             	 	 <th>RECEPÇÃO</th>
+             	 	 <th>MICROFONE VOLANTE</th>
+             	 	 <th>PORTAS e JANELAS</th>
+             	 	 <th>CORTINAS</th>
+             	 	 <th>SOM </th>
+             	 	 <th>DATA-SHOW</th>
+             	 	 <th>FILMAGEM</th>
+             	 	 <th>FOTOGRAFIA</th>
+             	 	 <th>GRUPO DE LOUVOR</th>
+             	 	 <th>RELATÓRIO DOS CULTOS</th>
+            	 </thead>
+             	 <tr>
+            	 	 <td>02/10/2016</td>
+            	 	 <td>Glaylton</td>
+            	 	 <td>Angelina e Ana Luiza</td>
+            	 	 <td>Kailaine</td>
+            	 	 <td>Lídia</td>
+            	 	 <td>Yasmin</td>
+            	 	 <td>Janderson e Bruno</td>
+            	 	 <td>Matheus Américo</td>
+            	 	 <td>Lídia</td>
+            	 	 <td>Isabella</td>
+            	 	 <td>Grupo Ágape</td>
+            	 	 <td>Emerson</td>
+             	 </tr>
+             	 <tr>
+            	 	 <td>09/10/2016</td>
+            	 	 <td>Luciano</td>
+            	 	 <td>Lourdes e Eva</td>
+            	 	 <td>Letícia</td>
+            	 	 <td>Danielle Parra</td>
+            	 	 <td>Luiz Fernando</td>
+            	 	 <td>Bruno e Felipe B.</td>
+            	 	 <td>Lídia</td>
+            	 	 <td>Felipe Paiva</td>
+            	 	 <td>Victória Martha</td>
+            	 	 <td>Jovens</td>
+            	 	 <td>Janderson</td>
+             	 </tr>
+             	 <tr>
+            	 	 <td>16/10/2016</td>
+            	 	 <td>Gilvan</td>
+            	 	 <td>Kailaine e Lídia</td>
+            	 	 <td>Matheus Souza</td>
+            	 	 <td>Luiz Fernando</td>
+            	 	 <td>Gustavo</td>
+            	 	 <td>Felipe B e Éder</td>
+            	 	 <td>Letícia</td>
+            	 	 <td>Janderson</td>
+            	 	 <td>Victória Al.</td>
+            	 	 <td>Casados</td>
+            	 	 <td>Luciano</td>
+             	 </tr>
+             	 <tr>
+            	 	 <td>23/10/2016</td>
+            	 	 <td>Flávio</td>
+            	 	 <td>Victória Martha e Clarice</td>
+            	 	 <td>Isabella</td>
+            	 	 <td>Lídia</td>
+            	 	 <td>Kailaine</td>
+            	 	 <td>Éder e Rafael</td>
+            	 	 <td>Beatriz</td>
+            	 	 <td>José Antônio</td>
+            	 	 <td>Lídia</td>
+            	 	 <td>Pastores</td>
+            	 	 <td>Israel</td>
+             	 </tr>
+             	 <tr>
+            	 	 <td>30/10/2016</td>
+            	 	 <td>Fernando</td>
+            	 	 <td>Beth e Eva</td>
+            	 	 <td>Victória Martha</td>
+            	 	 <td>Danielle Parra</td>
+            	 	 <td>Yasmin</td>
+            	 	 <td>Rafael e Janderson</td>
+            	 	 <td>Gabriela Al.</td>
+            	 	 <td>Felipe Paiva</td>
+            	 	 <td>Isabella</td>
+            	 	 <td>Grupo Ágape</td>
+            	 	 <td>Emerson</td>
+             	 </tr>
             </table>
         </div>
     </div>
