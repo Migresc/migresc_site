055ff3d4f284dc63268532b0901d1da7d36da7ac
Bruno Damasceno Martins <bruno@debian>
2016-12-10 22:14:03 -0200

    Atualização

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 ﻿<?php include 'header.html';?>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Seja bem vindo!</h3>
+        <h3 class="panel-title">Irmã Celina!</h3>
     </div>
     <div class="panel-body">
         <h3 >
-            Seja bem vindo!</p>
+            A irmã Celina encontra-se em uma casa de repouso no Parque do Carmo.</p>
+            As visitas podem ser feitas de quarta à domingo a partir das 15h até 17h</p>
+            O endereço é Rua Roberto de Almeida, 276.</p>
         </h3>
     </div>
 </div>
