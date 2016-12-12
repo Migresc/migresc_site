e0f8506a8d8a8c8ebaeec873f50d58edacec0a4a
BrunoDM2943 <bdm2943@gmail.com>
2016-12-12 19:55:46 -0200

    Atualizacao

--- eventos.php
+++ eventos.php
@@ -6,9 +6,10 @@
 	<div class="panel-body">
 		<div class="list-group">
 		<br>
-			<h4 class="list-group-item-heading">Almoço dos adolescentes</h4>
-			<p class="list-group-item-text">Local: IEPEM</p>
-			<p class="list-group-item-text">Data: 04/12 - Logo após a EBD</p>
+			<h4 class="list-group-item-heading">Culto de 31 de Dezembro</h4>
+			<p class="list-group-item-text">Batismos</p>
+			<p class="list-group-item-text">Assembléia</p>
+			<p class="list-group-item-text">Minuto Espiritual - Cada família deverá fazer uma apresentação (louvor, leitura da palavra, testemunho ou alguma palavra)</p>
 		<br>
         <h4 class="list-group-item-heading">Amigo Secreto</h4>
         <p class="list-group-item-text">Data: ???</p>
