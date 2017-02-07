d64d545a5b1542a9710270e80ea24bf362945244
BrunoDM2943 <bdm2943@gmail.com>
2017-02-07 21:52:27 -0200

    Atualizacao com as fotos do casamento da betania e do juan

---
+++ fotos-casamento-betania-juan.php
@@ -0,0 +1,20 @@
+<?php include 'header.html';?>
+<script type="text/javascript">
+	$( function() {
+		$( '#gallery' ).jGallery();
+	} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Fotos do casamento da Betânia e do Juan, primos do Pr. Celso e do irmão Manassés</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+			<a href="fotos/2015/casamento-betania-juan/1.jpg"> <img src="fotos/2015/casamento-betania-juan/1.jpg" alt="1"/></a>
+			<a href="fotos/2015/casamento-betania-juan/2.jpg"> <img src="fotos/2015/casamento-betania-juan/2.jpg" alt="2"/></a>
+			<a href="fotos/2015/casamento-betania-juan/3.jpg"> <img src="fotos/2015/casamento-betania-juan/3.jpg" alt="3"/></a>
+			<a href="fotos/2015/casamento-betania-juan/4.jpg"> <img src="fotos/2015/casamento-betania-juan/4.jpg" alt="4"/></a>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html';?>
