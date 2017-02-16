7b1831b48b7b0879a6b729ab06f4f3fdd224d8c8
BrunoDM2943 <bdm2943@gmail.com>
2017-02-15 22:44:39 -0200

    Atualização do site

--- escalaGeral.php
+++ escalaGeral.php
@@ -2,7 +2,7 @@
 <div class="panel panel-info">
     <!-- Default panel contents -->
     <div class="panel-heading">
-        <h3 class="panel-title">Dezembro/2016 - Manhã</h3>
+        <h3 class="panel-title">Fevereiro/2017 - Manhã</h3>
     </div>
     <div class="panel-body">
         <div class="table-responsive">
@@ -11,66 +11,74 @@
         	 <thead>
          	 	 <th>DATA</th>
          	 	 <th>PORTAS</th>
-         	 	 <th>RECEPÇÃO</th>
+         	 	 <th>RECEPÇÃO EXTERNA</th>
+         	 	 <th>SEGURANÇA E PORTARIA</th>
          	 	 <th>LOUVOR INFANTIL EBD</th>
          	 	 <th>VERSÍCULO DO MÊS - CLASSE</th>
          	 	 <th>ABERTURA EBD</th>
-         	 	 <th>LOUSA/PINCEL ETC</th>
+         	 	 <th>SECRETARIA (LOUSA/PINCEL/ETC)</th>
          	 	 <th>LANCHE JD. LAPENA</th>
-         	 	 <th>MONITOR JD. MARILÚ</th>
          	 	 <th>COZINHA</th>
          	 	 <th>LANCHE SERVIDO</th>
         	 </thead>
          	 <tr>
-        	 	 <td>04/12/16</td>
-        	 	 <td>Rebeca Hilary</td>
+        	 	 <td>05/02/2017</td>
+        	 	 <td>Luiz Fernando</td>
+        	 	 <td>Yasmin</td>
+        	 	 <td>Edgar</td>
+        	 	 <td>"Gabriela Al.</td>
+        	 	 <td> Guilherme e Rodrigo"</td>
+        	 	 <td>Cordeirinhos de Jesus</td>
+        	 	 <td>Janderson</td>
+        	 	 <td>Gabriela Bezerra</td>
         	 	 <td>Felipe Al.</td>
-        	 	 <td>Gabriela Al., Danielle, Guilherme</td>
-        	 	 <td>Adolescentes</td>
-        	 	 <td>Moisés</td>
-        	 	 <td>Kailaine</td>
-        	 	 <td>Rodrigo</td>
-        	 	 <td>Emerson</td>
-        	 	 <td>Cleonice, Luiza, José Antônio</td>
+        	 	 <td>"Cleonice</td>
+        	 	 <td> Lindália e Ana Luiza"</td>
         	 	 <td>Mortadela</td>
          	 </tr>
          	 <tr>
-        	 	 <td>11/12/16</td>
-        	 	 <td>Matheus Américo</td>
-        	 	 <td>Rafaela</td>
-        	 	 <td>Luiza, Danielle, Celso (não estará)</td>
-        	 	 <td>Adolescentes</td>
+        	 	 <td>12/02/2017</td>
+        	 	 <td>Rebeca Hilary</td>
+        	 	 <td>Angelina</td>
+        	 	 <td>José Francisco</td>
+        	 	 <td>"Luiza</td>
+        	 	 <td> Celso e Rodrigo"</td>
+        	 	 <td>Cordeirinhos de Jesus</td>
         	 	 <td>Emerson</td>
-        	 	 <td>Yasmin</td>
-        	 	 <td>Débora Cris.</td>
-        	 	 <td>Felipe Paiva</td>
-        	 	 <td>Ana Luzia, Neusa, Eva</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Debóra C.</td>
+        	 	 <td>"José Antônio</td>
+        	 	 <td> Eva e Ednéia"</td>
         	 	 <td>Salsicha</td>
          	 </tr>
          	 <tr>
-        	 	 <td>18/12/16</td>
-        	 	 <td>Rebeca Hilary</td>
-        	 	 <td>Dulcinéia</td>
-        	 	 <td>Gabriela Al., Danielle, Guilherme</td>
-        	 	 <td>Adolescentes</td>
-        	 	 <td>Luciano</td>
-        	 	 <td>Victória Martha</td>
-        	 	 <td>Altemir</td>
-        	 	 <td>Fernando</td>
-        	 	 <td>Angelina, Cristian, José Antônio</td>
+        	 	 <td>19/02/2017</td>
+        	 	 <td>Ana Luiza</td>
+        	 	 <td>Ciça</td>
+        	 	 <td>Glaylton</td>
+        	 	 <td>"Gabriela Al.</td>
+        	 	 <td> Guilherme e Rodrigo"</td>
+        	 	 <td>Cordeirinhos de Jesus</td>
+        	 	 <td>Manassés</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Francisco</td>
+        	 	 <td>"Eva</td>
+        	 	 <td> Lindália e Angelina"</td>
         	 	 <td>Carne Vermelha</td>
          	 </tr>
          	 <tr>
-        	 	 <td>25/12/16</td>
-        	 	 <td>Matheus Américo</td>
+        	 	 <td>26/02/2017</td>
+        	 	 <td>Luiz Fernando</td>
+        	 	 <td>Letícia</td>
         	 	 <td>Altemir</td>
-        	 	 <td>Luiza, Danielle, Celso (não estará)</td>
-        	 	 <td>Adolescentes</td>
-        	 	 <td>Manassés</td>
-        	 	 <td>Kailaine</td>
-        	 	 <td>Francisco</td>
-        	 	 <td>José Antônio</td>
-        	 	 <td>Luiza, Cleonice, Neusa</td>
+        	 	 <td>"Luiza</td>
+        	 	 <td> Celso e Rodrigo (RETIRO)"</td>
+        	 	 <td>Cordeirinhos de Jesus</td>
+        	 	 <td>Moisés</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Altemir</td>
+        	 	 <td>"Rosana</td>
+        	 	 <td> Cleonice e Rosemeire"</td>
         	 	 <td>Bolo</td>
          	 </tr>
         </table>
@@ -80,20 +88,21 @@
 </p>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Dezembro/2016 - Noite</h3>
+        <h3 class="panel-title">Fevereiro/2017 - Noite</h3>
     </div>
 
     <div class="panel-body">
         <div class="table-responsive">
             <!-- Table -->
             <table class="table">
-            	 <thead>  
+            	 <thead>
              	 	 <th>DATA</th>
-             	 	 <th>PORTARIA</th>
-             	 	 <th>RECEPÇÃO</th>
+             	 	 <th>SEGURANÇA E PORTARIA</th>
+             	 	 <th>RECEPÇÃO EXTERNA</th>
              	 	 <th>MICROFONE VOLANTE</th>
-             	 	 <th>PORTAS e JANELAS</th>
-             	 	 <th>CORTINAS</th>
+             	 	 <th>"PORTAS</th>
+             	 	 <th> JANELAS e CORTINAS"</th>
+             	 	 <th>RECEPÇÃO INTERNA</th>
              	 	 <th>SOM </th>
              	 	 <th>DATA-SHOW</th>
              	 	 <th>FILMAGEM</th>
@@ -102,63 +111,63 @@
              	 	 <th>RELATÓRIO DOS CULTOS</th>
             	 </thead>
              	 <tr>
-            	 	 <td>04/12/16</td>
-            	 	 <td>Gilvan</td>
-            	 	 <td>Angelina e Victória Martha</td>
-            	 	 <td>Isabella</td>
-            	 	 <td>Danielle Parra</td>
-            	 	 <td>Yasmin </td>
-            	 	 <td>Rafael e Janderson</td>
-            	 	 <td>Gabriela Al.</td>
-            	 	 <td>Felipe Paiva</td>
-            	 	 <td>Victória Al.</td>
-            	 	 <td>Jovens</td>
-            	 	 <td>Emerson</td>
-             	 </tr>
-             	 <tr>
-            	 	 <td>11/12/16</td>
-            	 	 <td>Flávio</td>
-            	 	 <td>Letícia e Kailaine</td>
-            	 	 <td>Victória Martha</td>
+            	 	 <td>05/02/2017</td>
+            	 	 <td>Altemir</td>
+            	 	 <td>Clarice e Victória Martha</td>
+            	 	 <td> </td>
             	 	 <td>Luiz Fernando</td>
-            	 	 <td>Gustavo </td>
-            	 	 <td>Janderson e Bruno</td>
-            	 	 <td>Matheus Américo</td>
+            	 	 <td>Bete e Andréia P.</td>
+            	 	 <td>Éder e Felipe B.</td>
+            	 	 <td>Bruno D.</td>
             	 	 <td>José Antônio</td>
-            	 	 <td>Isabella</td>
-            	 	 <td>Casados</td>
-            	 	 <td>Luciano</td>
-             	 </tr>
-             	 <tr>
-            	 	 <td>18/12/16</td>
-            	 	 <td>Fernando</td>
-            	 	 <td>Clarice e Eva</td>
-            	 	 <td>Gabriela Bezerra</td>
-            	 	 <td>Danielle Parra</td>
-            	 	 <td>Isabella </td>
-            	 	 <td>Bruno e Felipe B.</td>
             	 	 <td>Letícia</td>
-            	 	 <td>Lídia</td>
-            	 	 <td>Victória Martha</td>
-            	 	 <td>Pastores</td>
-            	 	 <td>Israel</td>
+            	 	 <td>Casados</td>
+            	 	 <td> </td>
              	 </tr>
              	 <tr>
-            	 	 <td>25/12/16</td>
-            	 	 <td>Francisco</td>
-            	 	 <td>Beth e Ana Luiza</td>
-            	 	 <td>Kailaine</td>
-            	 	 <td>Luiz Fernando</td>
-            	 	 <td>Vinícius </td>
-            	 	 <td>Felipe B. e Éder</td>
-            	 	 <td>Lídia</td>
-            	 	 <td>Janderson</td>
-            	 	 <td>Lídia</td>
+            	 	 <td>12/02/2017</td>
+            	 	 <td>Glaylton</td>
+            	 	 <td>Letícia e Flávia</td>
+            	 	 <td> </td>
+            	 	 <td>Rebeca Hilary</td>
+            	 	 <td>Bete e Andréia P.</td>
+            	 	 <td>Janderson e Rafael </td>
+            	 	 <td>Beatriz </td>
+            	 	 <td>Felipe Paiva </td>
+            	 	 <td>Gabriela Al. </td>
+            	 	 <td>Pastores</td>
+            	 	 <td> </td>
+             	 </tr>
+             	 <tr>
+            	 	 <td>19/02/2017</td>
+            	 	 <td>Gilvan</td>
+            	 	 <td>Eva e Rafaela</td>
+            	 	 <td> </td>
+            	 	 <td>Ana Luiza</td>
+            	 	 <td>Bete e Andréia P.</td>
+            	 	 <td> Éder e Felipe B.</td>
+            	 	 <td>Gabriela Al. </td>
+            	 	 <td>Rafael </td>
+            	 	 <td>Beatriz </td>
             	 	 <td>Grupo Ágape</td>
-            	 	 <td>Janderson</td>
+            	 	 <td> </td>
+             	 </tr>
+             	 <tr>
+            	 	 <td>26/02/2017</td>
+            	 	 <td>Edgar</td>
+            	 	 <td>Victória Almeida e Isabella</td>
+            	 	 <td> </td>
+            	 	 <td>Luiz Fernado</td>
+            	 	 <td>Bete e Andréia P.</td>
+            	 	 <td> Bruno e Janderson</td>
+            	 	 <td>Letícia </td>
+            	 	 <td>José Antônio</td>
+            	 	 <td>Beatriz</td>
+            	 	 <td>Jovens</td>
+            	 	 <td> </td>
              	 </tr>
             </table>
-        </div>
+          </div>
     </div>
 </div>
 <?php include 'footer.html';?>
