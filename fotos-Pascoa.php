ab9b4a3f4aa29211d8ec51543d341dac1f7e9520
BrunoDM2943 <bdm2943@gmail.com>
2017-05-01 21:54:14 -0300

    Atualizações

---
+++ fotos-Pascoa.php
@@ -0,0 +1,91 @@
+
+<?php include 'header.html';?>
+<script type="text/javascript">
+	$( function() {
+		$( '#gallery' ).jGallery();
+	} );
+</script>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Fotos do Batismo</h3>
+	</div>
+	<div class="panel-body">
+		<div id="gallery">
+       <a href="fotos/2017/Pascoa/DSC03980.JPG"> <img src="fotos/2017/Pascoa/DSC03980.JPG" alt="1"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03999.JPG"> <img src="fotos/2017/Pascoa/DSC03999.JPG" alt="2"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03959.JPG"> <img src="fotos/2017/Pascoa/DSC03959.JPG" alt="3"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03960.JPG"> <img src="fotos/2017/Pascoa/DSC03960.JPG" alt="4"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03961.JPG"> <img src="fotos/2017/Pascoa/DSC03961.JPG" alt="5"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03962.JPG"> <img src="fotos/2017/Pascoa/DSC03962.JPG" alt="6"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03963.JPG"> <img src="fotos/2017/Pascoa/DSC03963.JPG" alt="7"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03964.JPG"> <img src="fotos/2017/Pascoa/DSC03964.JPG" alt="8"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03965.JPG"> <img src="fotos/2017/Pascoa/DSC03965.JPG" alt="9"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03966.JPG"> <img src="fotos/2017/Pascoa/DSC03966.JPG" alt="10"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03967.JPG"> <img src="fotos/2017/Pascoa/DSC03967.JPG" alt="11"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03968.JPG"> <img src="fotos/2017/Pascoa/DSC03968.JPG" alt="12"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03969.JPG"> <img src="fotos/2017/Pascoa/DSC03969.JPG" alt="13"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03970.JPG"> <img src="fotos/2017/Pascoa/DSC03970.JPG" alt="14"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03971.JPG"> <img src="fotos/2017/Pascoa/DSC03971.JPG" alt="15"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03972.JPG"> <img src="fotos/2017/Pascoa/DSC03972.JPG" alt="16"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03973.JPG"> <img src="fotos/2017/Pascoa/DSC03973.JPG" alt="17"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03974.JPG"> <img src="fotos/2017/Pascoa/DSC03974.JPG" alt="18"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03975.JPG"> <img src="fotos/2017/Pascoa/DSC03975.JPG" alt="19"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03976.JPG"> <img src="fotos/2017/Pascoa/DSC03976.JPG" alt="20"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03977.JPG"> <img src="fotos/2017/Pascoa/DSC03977.JPG" alt="21"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03978.JPG"> <img src="fotos/2017/Pascoa/DSC03978.JPG" alt="22"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03979.JPG"> <img src="fotos/2017/Pascoa/DSC03979.JPG" alt="23"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03981.JPG"> <img src="fotos/2017/Pascoa/DSC03981.JPG" alt="24"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03982.JPG"> <img src="fotos/2017/Pascoa/DSC03982.JPG" alt="25"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03983.JPG"> <img src="fotos/2017/Pascoa/DSC03983.JPG" alt="26"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03985.JPG"> <img src="fotos/2017/Pascoa/DSC03985.JPG" alt="27"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03986.JPG"> <img src="fotos/2017/Pascoa/DSC03986.JPG" alt="28"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03987.JPG"> <img src="fotos/2017/Pascoa/DSC03987.JPG" alt="29"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03988.JPG"> <img src="fotos/2017/Pascoa/DSC03988.JPG" alt="30"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03989.JPG"> <img src="fotos/2017/Pascoa/DSC03989.JPG" alt="31"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03990.JPG"> <img src="fotos/2017/Pascoa/DSC03990.JPG" alt="32"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03991.JPG"> <img src="fotos/2017/Pascoa/DSC03991.JPG" alt="33"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03992.JPG"> <img src="fotos/2017/Pascoa/DSC03992.JPG" alt="34"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03993.JPG"> <img src="fotos/2017/Pascoa/DSC03993.JPG" alt="35"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03994.JPG"> <img src="fotos/2017/Pascoa/DSC03994.JPG" alt="36"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03995.JPG"> <img src="fotos/2017/Pascoa/DSC03995.JPG" alt="37"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03996.JPG"> <img src="fotos/2017/Pascoa/DSC03996.JPG" alt="38"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03997.JPG"> <img src="fotos/2017/Pascoa/DSC03997.JPG" alt="39"/></a> 
+       <a href="fotos/2017/Pascoa/DSC03998.JPG"> <img src="fotos/2017/Pascoa/DSC03998.JPG" alt="40"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04000.JPG"> <img src="fotos/2017/Pascoa/DSC04000.JPG" alt="41"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04001.JPG"> <img src="fotos/2017/Pascoa/DSC04001.JPG" alt="42"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04002.JPG"> <img src="fotos/2017/Pascoa/DSC04002.JPG" alt="43"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04003.JPG"> <img src="fotos/2017/Pascoa/DSC04003.JPG" alt="44"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04006.JPG"> <img src="fotos/2017/Pascoa/DSC04006.JPG" alt="45"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04007.JPG"> <img src="fotos/2017/Pascoa/DSC04007.JPG" alt="46"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04009.JPG"> <img src="fotos/2017/Pascoa/DSC04009.JPG" alt="47"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04010.JPG"> <img src="fotos/2017/Pascoa/DSC04010.JPG" alt="48"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04011.JPG"> <img src="fotos/2017/Pascoa/DSC04011.JPG" alt="49"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04013.JPG"> <img src="fotos/2017/Pascoa/DSC04013.JPG" alt="50"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04014.JPG"> <img src="fotos/2017/Pascoa/DSC04014.JPG" alt="51"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04015.JPG"> <img src="fotos/2017/Pascoa/DSC04015.JPG" alt="52"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04016.JPG"> <img src="fotos/2017/Pascoa/DSC04016.JPG" alt="53"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04017.JPG"> <img src="fotos/2017/Pascoa/DSC04017.JPG" alt="54"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04018.JPG"> <img src="fotos/2017/Pascoa/DSC04018.JPG" alt="55"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04019.JPG"> <img src="fotos/2017/Pascoa/DSC04019.JPG" alt="56"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04020.JPG"> <img src="fotos/2017/Pascoa/DSC04020.JPG" alt="57"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04021.JPG"> <img src="fotos/2017/Pascoa/DSC04021.JPG" alt="58"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04022.JPG"> <img src="fotos/2017/Pascoa/DSC04022.JPG" alt="59"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04023.JPG"> <img src="fotos/2017/Pascoa/DSC04023.JPG" alt="60"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04024.JPG"> <img src="fotos/2017/Pascoa/DSC04024.JPG" alt="61"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04025.JPG"> <img src="fotos/2017/Pascoa/DSC04025.JPG" alt="62"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04026.JPG"> <img src="fotos/2017/Pascoa/DSC04026.JPG" alt="63"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04027.JPG"> <img src="fotos/2017/Pascoa/DSC04027.JPG" alt="64"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04028.JPG"> <img src="fotos/2017/Pascoa/DSC04028.JPG" alt="65"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04029.JPG"> <img src="fotos/2017/Pascoa/DSC04029.JPG" alt="66"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04031.JPG"> <img src="fotos/2017/Pascoa/DSC04031.JPG" alt="67"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04032.JPG"> <img src="fotos/2017/Pascoa/DSC04032.JPG" alt="68"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04033.JPG"> <img src="fotos/2017/Pascoa/DSC04033.JPG" alt="69"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04034.JPG"> <img src="fotos/2017/Pascoa/DSC04034.JPG" alt="70"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04036.JPG"> <img src="fotos/2017/Pascoa/DSC04036.JPG" alt="71"/></a> 
+       <a href="fotos/2017/Pascoa/DSC04038.JPG"> <img src="fotos/2017/Pascoa/DSC04038.JPG" alt="72"/></a> 
+
+
+</div>
+</div>
+</div>
+<?php include 'footer.html';?>
