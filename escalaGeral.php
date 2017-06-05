9122c5612dc5ec87ba1aace6fba6f05275056621
BrunoDM2943 <bdm2943@gmail.com>
2017-06-05 20:57:32 -0300

    Atualização da escala

--- escalaGeral.php
+++ escalaGeral.php
@@ -2,189 +2,189 @@
 <div class="panel panel-info">
     <!-- Default panel contents -->
     <div class="panel-heading">
-        <h3 class="panel-title">Maio/2017 - Manhã</h3>
+        <h3 class="panel-title">Junho/2017 - Manhã</h3>
     </div>
     <div class="panel-body">
         <div class="table-responsive">
         <!-- Table -->
         <table class="table">
-	 <thead>
- 	 	 <th>RESPONSÁVEL</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Danielle</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Valéria</th>
- 	 	 <th>Professores</th>
- 	 	 <th>Janderson</th>
- 	 	 <th>Gabriela B.</th>
- 	 	 <th>Adelaide</th>
- 	 	 <th>Adelaide</th>
- 	 	 <th>Adelaide</th>
-	 </thead>
- 	 <tr>
-	 	 <td>DATA</td>
-	 	 <td>PORTAS</td>
-	 	 <td>RECEPÇÃO EXTERNA</td>
-	 	 <td>SEGURANÇA E PORTARIA</td>
-	 	 <td>LOUVOR INFANTIL EBD</td>
-	 	 <td>VERSÍCULOS DO MÊS - CLASSE</td>
-	 	 <td>ABERTURA EBD</td>
-	 	 <td>SECRETARIA (LOUSA/PINCEL/ETC)</td>
-	 	 <td>LANCHE JD. LAPENA</td>
-	 	 <td>COZINHA</td>
-	 	 <td>LANCHE SERVIDO</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>07/05/17</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Yasmin</td>
-	 	 <td>Edgar</td>
-	 	 <td>Luiza, Celso e Rodrigo</td>
-	 	 <td>Cordeirinhos de Jesus</td>
-	 	 <td>Emerson</td>
-	 	 <td>Gabriela Bezerra</td>
-	 	 <td>Débora C.</td>
-	 	 <td>Ana Luiza, Eva, José Antônio</td>
-	 	 <td>Mortadela</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>14/05/17</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td>Cissa</td>
-	 	 <td>Gilvan</td>
-	 	 <td>Gabriela Al., Guilherme e Rodrigo</td>
-	 	 <td>Cordeirinhos de Jesus</td>
-	 	 <td>Manassés</td>
-	 	 <td>Gabriela Bezerra</td>
-	 	 <td>Altemir</td>
-	 	 <td>Lindália, Neusa, Cleonice</td>
-	 	 <td>Salsicha</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>21/05/17</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Letícia</td>
-	 	 <td>Glaylton</td>
-	 	 <td>Luiza, Celso e Rodrigo</td>
-	 	 <td>Cordeirinhos de Jesus</td>
-	 	 <td>Moisés</td>
-	 	 <td>Gabriela Bezerra</td>
-	 	 <td>Gabriela Al.</td>
-	 	 <td>Rosana, Eva, Ednéia</td>
-	 	 <td>Frango</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>28/05/17</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Yasmin</td>
-	 	 <td>Altemir</td>
-	 	 <td>Gabriela Al., Guilherme e Rodrigo</td>
-	 	 <td>Cordeirinhos de Jesus</td>
-	 	 <td>Janderson</td>
-	 	 <td>Gabriela Bezerra</td>
-	 	 <td>Felipe Al.</td>
-	 	 <td>Rosemeire, Cleonice, Neusa</td>
-	 	 <td>Bolo</td>
- 	 </tr>
-</table>
+        	 <thead>
+         	 	 <th>Responsável</th>
+         	 	 <th>Moisés</th>
+         	 	 <th>Danielle</th>
+         	 	 <th>Moisés</th>
+         	 	 <th>Valéria</th>
+         	 	 <th>Professores</th>
+         	 	 <th>Janderson</th>
+         	 	 <th>Gabriela B.</th>
+         	 	 <th>Adelaide</th>
+         	 	 <th>Adelaide</th>
+         	 	 <th>Adelaide</th>
+        	 </thead>
+         	 <tr>
+        	 	 <td>DATA</td>
+        	 	 <td>PORTAS</td>
+        	 	 <td>RECEPÇÃO EXTERNA</td>
+        	 	 <td>SEGURANÇA E PORTARIA</td>
+        	 	 <td>LOUVOR INFANTIL EBD</td>
+        	 	 <td>VERSÍCULOS DO MÊS - CLASSE</td>
+        	 	 <td>ABERTURA EBD</td>
+        	 	 <td>SECRETARIA (LOUSA/PINCEL/ETC)</td>
+        	 	 <td>LANCHE JD. LAPENA</td>
+        	 	 <td>COZINHA</td>
+        	 	 <td>LANCHE SERVIDO</td>
+         	 </tr>
+         	 <tr>
+        	 	 <td>4/6/2017</td>
+        	 	 <td>Rebeca Hilary</td>
+        	 	 <td>Yasmin</td>
+        	 	 <td>Gilvan</td>
+        	 	 <td>Luiza, Celso e Rodrigo</td>
+        	 	 <td>Videiras</td>
+        	 	 <td>Janderson</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Altemir</td>
+        	 	 <td>Cristian, Ana Luiza, Lindália</td>
+        	 	 <td>Mortadela</td>
+         	 </tr>
+         	 <tr>
+        	 	 <td>11/6/2017</td>
+        	 	 <td>Luiz Fernando</td>
+        	 	 <td>Angelina</td>
+        	 	 <td>Edgar</td>
+        	 	 <td>Gabriela Al., Guilherme e Rodrigo</td>
+        	 	 <td>Videiras</td>
+        	 	 <td>Emerson</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Guilherme</td>
+        	 	 <td>Neusa, Eva, José Antônio</td>
+        	 	 <td>Salsicha</td>
+         	 </tr>
+         	 <tr>
+        	 	 <td>18/6/2017</td>
+        	 	 <td>Luiz Fernando</td>
+        	 	 <td>Cissa</td>
+        	 	 <td>Altemir</td>
+        	 	 <td>Luiza, Celso e Rodrigo</td>
+        	 	 <td>Videiras</td>
+        	 	 <td>Manassés</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Gabriela Al.</td>
+        	 	 <td>Ednéia, Lindália, José Antônio</td>
+        	 	 <td>Carne Vermelha </td>
+         	 </tr>
+         	 <tr>
+        	 	 <td>25/6/2017</td>
+        	 	 <td>Rebeca Hilary</td>
+        	 	 <td>Letícia</td>
+        	 	 <td>Gilvan</td>
+        	 	 <td>Gabriela Al., Guilherme e Rodrigo</td>
+        	 	 <td>Videiras</td>
+        	 	 <td>Moisés</td>
+        	 	 <td>Gabriela Bezerra</td>
+        	 	 <td>Francisco</td>
+        	 	 <td>Rosemeire, Neusa ou Cleonice, Eva</td>
+        	 	 <td>Bolo</td>
+         	 </tr>
+        </table>
         </div>
     </div>
 </div>
 </p>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Maio/2017 - Noite</h3>
+        <h3 class="panel-title">Junho/2017 - Noite</h3>
     </div>
 
     <div class="panel-body">
         <div class="table-responsive">
             <!-- Table -->
             <table class="table">
-	 <thead>
- 	 	 <th>RESPONSÁVEL</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Danielle</th>
- 	 	 <th>Presbitério</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Luiza</th>
- 	 	 <th>Não terá</th>
-	 </thead>
- 	 <tr>
-	 	 <td>DATA</td>
-	 	 <td>SEGURANÇA E PORTARIA</td>
-	 	 <td>RECEPÇÃO EXTERNA</td>
-	 	 <td>MICROFONE VOLANTE</td>
-	 	 <td>PORTAS, JANELAS E CORTINAS</td>
-	 	 <td>RECEPÇÃO INTERNA</td>
-	 	 <td>SOM</td>
-	 	 <td>DATA-SHOW</td>
-	 	 <td>FILMAGEM</td>
-	 	 <td>FOTOGRAFIA</td>
-	 	 <td>GRUPO DE LOUVOR</td>
-	 	 <td>RELATÓRIO DOS CULTOS</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>07/05/17</td>
-	 	 <td>Glailton</td>
-	 	 <td>Victória Martha</td>
-	 	 <td>Não terá</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Janderson e Rafael</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Felipe Paiva</td>
-	 	 <td>Gabriela Al.</td>
-	 	 <td>Pastores</td>
-	 	 <td>Não terá</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>14/05/17</td>
-	 	 <td>Sidney</td>
-	 	 <td>Isabella e Ana Luiza</td>
-	 	 <td>Não terá</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Éder e Felipe B.</td>
-	 	 <td>Gabriela Al.</td>
-	 	 <td>Rafael</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Grupo Ágape</td>
-	 	 <td>Não terá</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>21/05/17</td>
-	 	 <td>Gilvan</td>
-	 	 <td>Eva</td>
-	 	 <td>Não terá</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Bruno e Janderson</td>
-	 	 <td>Letícia</td>
-	 	 <td>José Antônio</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Jovens</td>
-	 	 <td>Não terá</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>28/05/17</td>
-	 	 <td>Altemir</td>
-	 	 <td>Victória Al.</td>
-	 	 <td>Não terá</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Felipe B. e Éder</td>
-	 	 <td>Gabriela Al.</td>
-	 	 <td>Janderson</td>
-	 	 <td>Felipe Paiva</td>
-	 	 <td>Casados</td>
-	 	 <td>Não terá</td>
- 	 </tr>
-</table>
+          	 <thead>
+           	 	 <th>Responsável</th>
+           	 	 <th>Moisés</th>
+           	 	 <th>Danielle</th>
+           	 	 <th>Presbitério</th>
+           	 	 <th>Moisés</th>
+           	 	 <th>N/A</th>
+           	 	 <th>Kazuiti</th>
+           	 	 <th>Kazuiti</th>
+           	 	 <th>Kazuiti</th>
+           	 	 <th>Kazuiti</th>
+           	 	 <th>Luiza</th>
+           	 	 <th>N/A</th>
+          	 </thead>
+           	 <tr>
+          	 	 <td>DATA</td>
+          	 	 <td>SEGURANÇA E PORTARIA</td>
+          	 	 <td>RECEPÇÃO EXTERNA</td>
+          	 	 <td>MICROFONE VOLANTE</td>
+          	 	 <td>PORTAS, JANELAS E CORTINAS</td>
+          	 	 <td>RECEPÇÃO INTERNA</td>
+          	 	 <td>SOM</td>
+          	 	 <td>DATA-SHOW</td>
+          	 	 <td>FILMAGEM</td>
+          	 	 <td>FOTOGRAFIA</td>
+          	 	 <td>GRUPO DE LOUVOR</td>
+          	 	 <td>RELATÓRIO DOS CULTOS</td>
+           	 </tr>
+           	 <tr>
+          	 	 <td>4/6/2017</td>
+          	 	 <td>Sidney</td>
+          	 	 <td>Victória Martha e Ana Luiza</td>
+          	 	 <td>Rebeca Hilay</td>
+               <td>Não terá</td>
+          	 	 <td>Beth e Andréia Paiva</td>
+          	 	 <td>José Antônio e Rafael</td>
+          	 	 <td>Beatriz</td>
+          	 	 <td>Felipe Bernardo</td>
+          	 	 <td>Felipe Paiva</td>
+          	 	 <td>Pastores</td>
+          	 	 <td>Não terá</td>
+           	 </tr>
+           	 <tr>
+          	 	 <td>11/6/2017</td>
+          	 	 <td>Altemir</td>
+          	 	 <td>Eva</td>
+          	 	 <td>Luiz Fernando</td>
+               <td>Não terá</td>
+          	 	 <td>Beth e Andréia Paiva</td>
+          	 	 <td>Éder e Felipe Bernardo</td>
+          	 	 <td>Bruno</td>
+          	 	 <td>José Antônio</td>
+          	 	 <td>Letícia</td>
+          	 	 <td>Grupo Ágape</td>
+          	 	 <td>Não terá</td>
+           	 </tr>
+           	 <tr>
+          	 	 <td>18/6/2017</td>
+          	 	 <td>Gilvan</td>
+          	 	 <td>Angelina e Ana Luiza</td>
+          	 	 <td>Luiz Fernando</td>
+               <td>Não terá</td>
+          	 	 <td>Beth e Andréia Paiva</td>
+          	 	 <td>Éder e Janderson</td>
+          	 	 <td>Letícia</td>
+          	 	 <td>Felipe Paiva</td>
+          	 	 <td>Beatriz</td>
+          	 	 <td>Jovens</td>
+          	 	 <td>Não terá</td>
+           	 </tr>
+           	 <tr>
+          	 	 <td>25/6/2017</td>
+          	 	 <td>Glaylton</td>
+          	 	 <td>Isabela e Angelina</td>
+          	 	 <td>Rebeca Hilay</td>
+               <td>Não terá</td>
+          	 	 <td>Beth e Andréia Paiva</td>
+          	 	 <td>Rafael e Felipe Bernardo</td>
+          	 	 <td>Bruno </td>
+          	 	 <td>Beatriz</td>
+          	 	 <td>Felipe Bernardo</td>
+          	 	 <td>Casados</td>
+          	 	 <td>Não terá</td>
+           	 </tr>
+          </table>
           </div>
     </div>
 </div>
