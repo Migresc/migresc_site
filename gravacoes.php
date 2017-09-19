581df4f9971ab45f64426c0da8d9adcc416dd244
Bruno Damasceno Martins <bruno@Notebook.home>
2017-09-18 21:19:58 -0300

    Nova página de gravações

---
+++ gravacoes.php
@@ -0,0 +1,23 @@
+<?php include 'header.html';?>
+<div class="panel panel-info">
+	<div class="panel-heading">
+		<h3 class="panel-title">Gravações</h3>
+	</div>
+	<div class="panel-body">
+		<div class="list-group">
+			<div class="row">
+		  <div class="col-sm-6 col-md-4">
+		    <div class="thumbnail">
+		      <div class="caption">
+		        <h3>Violência e o Cristão</h3>
+		        <p>Pregador: Celso de Carvalho</p>
+						<p>Data: 17/09/17</p>
+		        <p><a href="https://www.youtube.com/watch?v=yFGdNL-nsnQ#action=share" target="_blank" class="btn btn-primary" role="button">Assistir</a></p>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html';?>
