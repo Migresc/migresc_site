35c90894668a1fd055a00a489a812c4a06a76acc
Bruno Damasceno Martins <bdm2943@gmail.com>
2017-10-15 11:52:17 -0200

    Atualização dos eventos!

--- eventos.php
+++ eventos.php
@@ -3,6 +3,12 @@
 		<h3 class="panel-title">Eventos <a class="glyphicon glyphicon-calendar"></a></h3>
 	</div>
 	<div class="panel-body">
-			Os próximos eventos estarão aqui!
+		<div class="list-group">
+			<a href="#" class="list-group-item active">
+				<h4 class="list-group-item-heading">Sexta-Feira - 20/10 - Culto de aniversário da IEP Sede</h4>
+				<p class="list-group-item-text">Sairemos da igreja às 19h00, pois o culto em Pinheiros começará às 20h.
+					<br> Confirmem presença</p>
+			</a>
+		</div>
 	</div>
 </div>
