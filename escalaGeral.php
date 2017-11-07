86f0277e156b25437318f67040275133c8749b0d
Bruno Damasceno Martins <bdm2943@gmail.com>
2017-11-06 22:18:10 -0200

    Escala de Novembro

--- escalaGeral.php
+++ escalaGeral.php
@@ -3,209 +3,167 @@
 <div class="panel panel-info">
     <!-- Default panel contents -->
     <div class="panel-heading">
-        <h3 class="panel-title">Outubro/2017 - Manhã</h3>
+        <h3 class="panel-title">Novembro/2017 - Manhã</h3>
     </div>
     <div class="panel-body">
         <div class="table-responsive">
         <!-- Table -->
         <table class="table">
-	 <thead>
- 	 	 <th>RESPONSÁVEL</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Danielle</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Valéria</th>
- 	 	 <th>Professores</th>
- 	 	 <th>Janderson</th>
- 	 	 <th>Gabriela B.</th>
-     <th>Gabriela B.</th>
- 	 	 <th>Adelaide</th>
- 	 	 <th>Adelaide</th>
- 	 	 <th>Adelaide</th>
-	 </thead>
- 	 <tr>
-	 	 <td>DATA</td>
-	 	 <td>PORTAS</td>
-	 	 <td>RECEPÇÃO EXTERNA</td>
-	 	 <td>SEGURANÇA E PORTARIA</td>
-	 	 <td>LOUVOR INFANTIL EBD</td>
-	 	 <td>VERSÍCULOS DO MÊS - CLASSE</td>
-	 	 <td>ABERTURA EBD</td>
-	 	 <td>SECRETARIA (LOUSA/PINCEL/ETC)</td>
-     <td>FARMÁCIA IEP</td>
-	 	 <td>LANCHE JD. LAPENA</td>
-	 	 <td>COZINHA</td>
-	 	 <td>LANCHE SERVIDO</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>01/10/17</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td></td>
-	 	 <td></td>
-	 	 <td>Gabriela Al., Guilheme e Rodrigo</td>
-	 	 <td>Videiras</td>
-	 	 <td>Janderson</td>
-	 	 <td>Gabriela Bezerra</td>
-     <td>Elizabeth B.</td>
-	 	 <td>Gabriela Al.</td>
-	 	 <td>Neuza, Ana Luiza, Rosemeire</td>
-	 	 <td>Mortadela</td>
- 	 </tr>
-   <tr>
-	 	 <td>08/10/17</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td></td>
-	 	 <td></td>
-	 	 <td>Luiza, Celso e Rodrigo</td>
-	 	 <td>Videiras</td>
-	 	 <td>Manassés</td>
-	 	 <td>Gabriela Bezerra</td>
-     <td>Elizabeth B.</td>
-	 	 <td>Rodrigo</td>
-	 	 <td>José Antônio, Ednéia</td>
-	 	 <td>Salsicha</td>
- 	 </tr>
-   <tr>
-	 	 <td>15/10/17</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td></td>
-	 	 <td></td>
-	 	 <td>Gabriela Al., Guilheme e Rodrigo</td>
-	 	 <td>Videiras</td>
-	 	 <td>Janderson</td>
-	 	 <td>Gabriela Bezerra</td>
-     <td>Elizabeth B.</td>
-	 	 <td>Felipe Al</td>
-	 	 <td>Cristian, Ana Luiza, Neuza</td>
-	 	 <td>Frango</td>
- 	 </tr>
-   <tr>
-	 	 <td>22/10/17</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td></td>
-	 	 <td></td>
-	 	 <td>Luiza, Celso e Rodrigo</td>
-	 	 <td>Videiras</td>
-	 	 <td>Manassés</td>
-	 	 <td>Gabriela Bezerra</td>
-     <td>Elizabeth B.</td>
-	 	 <td>Gabriela Al</td>
-	 	 <td>José Antônio, Ednéia</td>
-	 	 <td>Bolo de Cenoura</td>
- 	 </tr>
-   <tr>
-	 	 <td>29/10/17</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td></td>
-	 	 <td></td>
-	 	 <td>Gabriela Al., Guilheme e Rodrigo</td>
-	 	 <td>Videiras</td>
-	 	 <td>Janderson</td>
-	 	 <td>Gabriela Bezerra</td>
-     <td>Elizabeth B.</td>
-	 	 <td>Felipe Al</td>
-	 	 <td>Rosimeire, Neuza, Lindália</td>
-	 	 <td>Pão de patê</td>
- 	 </tr>
-</table>
+        	<thead>
+        	 	<th>DATA</th>
+        	 	<th>PORTAS</th>
+        	 	<th>RECEPÇÃO EXTERNA</th>
+        	 	<th>SEGURANÇA E PORTARIA</th>
+        	 	<th>LOUVOR INFANTIL EBD</th>
+        	 	<th>VERSÍCULOS DO MÊS - CLASSE</th>
+        	 	<th>ABERTURA EBD</th>
+        	 	<th>SECRETARIA (LOUSA/PINCEL/ETC)</th>
+        	 	<th>FARMÁCIA IEP</th>
+        	 	<th>LANCHE JD. LAPENA</th>
+        	 	<th>COZINHA</th>
+        	 	<th>LANCHE SERVIDO</th>
+        	</thead>
+        	<tr>
+        	 	<td>5/11/2017</td>
+        	 	<td>Rebeca Hilary</td>
+        	 	<td>N/A</td>
+        	 	<td>N/A</td>
+        	 	<td>Gabriela Al.</td>
+        	 	<td> Guilherme e Rodrigo</td>
+        	 	<td>Soldados de Rei</td>
+        	 	<td>Manassés</td>
+        	 	<td>Gabriela Bezerra</td>
+        	 	<td>Andréia Paiva</td>
+        	 	<td>Gabriela Al.</td>
+        	 	<td>Luiza</td>
+        	 	<td> Ednéia e José Antônio</td>
+        	 	<td>Mortadela</td>
+        	</tr>
+        	<tr>
+        	 	<td>12/11/2017</td>
+        	 	<td>Luiz Fernando</td>
+        	 	<td>N/A</td>
+        	 	<td>N/A</td>
+        	 	<td>Luiza</td>
+        	 	<td> Celso e Rodrigo</td>
+        	 	<td>Soldados de Rei</td>
+        	 	<td>Janderson</td>
+        	 	<td>Gabriela Bezerra</td>
+        	 	<td>Andréia Paiva</td>
+        	 	<td>Rodrigo</td>
+        	 	<td>Neusa</td>
+        	 	<td> José Antônio e Ednéia</td>
+        	 	<td>Salsicha</td>
+        	</tr>
+        	<tr>
+        	 	<td>19/11/2017</td>
+        	 	<td>Rebeca Hilary</td>
+        	 	<td>N/A</td>
+        	 	<td>N/A</td>
+        	 	<td>Gabriela Al.</td>
+        	 	<td> Guilherme e Rodrigo</td>
+        	 	<td>Soldados de Rei</td>
+        	 	<td>Manassés</td>
+        	 	<td>Gabriela Bezerra</td>
+        	 	<td>Andréia Paiva</td>
+        	 	<td>Felipe Al.</td>
+        	 	<td>Neusa</td>
+        	 	<td> Ednéia</td>
+        	 	<td> Luiza</td>
+        	 	<td>Frango</td>
+        	</tr>
+        	<tr>
+        	 	<td>26/11/2017</td>
+        	 	<td>Luiz Fernando</td>
+        	 	<td>N/A</td>
+        	 	<td>N/A</td>
+        	 	<td>Luiza</td>
+        	 	<td> Celso e Rodrigo</td>
+        	 	<td>Soldados de Rei</td>
+        	 	<td>Janderson</td>
+        	 	<td>Gabriela Bezerra</td>
+        	 	<td>Andréia Paiva</td>
+        	 	<td>Gabriela Al.</td>
+        	 	<td>Rose</td>
+        	 	<td> Cristian e Lindália</td>
+        	 	<td>Bolo de Côco</td>
+        	</tr>
+        </table>
         </div>
     </div>
 </div>
 </p>
 <div class="panel panel-info">
     <div class="panel-heading">
-        <h3 class="panel-title">Outubro/2017 - Noite</h3>
+        <h3 class="panel-title">Novembro/2017 - Noite</h3>
     </div>
 
     <div class="panel-body">
         <div class="table-responsive">
             <!-- Table -->
             <table class="table">
-	 <thead>
- 	 	 <th>RESPONSÁVEL</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Danielle</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Moisés</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Kazuiti</th>
- 	 	 <th>Luiza</th>
-	 </thead>
- 	 <tr>
-	 	 <td>DATA</td>
-	 	 <td>SEGURANÇA E PORTARIA</td>
-	 	 <td>RECEPÇÃO EXTERNA</td>
-	 	 <td>PORTAS, JANELAS E CORTINAS</td>
-	 	 <td>RECEPÇÃO INTERNA</td>
-	 	 <td>SOM</td>
-	 	 <td>DATA-SHOW</td>
-	 	 <td>FILMAGEM</td>
-	 	 <td>FOTOGRAFIA</td>
-	 	 <td>GRUPO DE LOUVOR</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>01/10/17</td>
-	 	 <td>Gilvan</td>
-	 	 <td>Isabella</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Éder e Felipe Bernardo</td>
-	 	 <td>Bruno Damasceno</td>
-	 	 <td>José Antônio</td>
-	 	 <td>Letícia</td>
-	 	 <td>Grupe Ágape</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>08/10/17</td>
-	 	 <td>Glaylton</td>
-	 	 <td>Victória Almeida</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Éder e Janderson</td>
-	 	 <td>Letícia</td>
-	 	 <td>Felipe Paiva</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Jovens</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>15/10/17</td>
-	 	 <td>Gilvan</td>
-	 	 <td>Victória Martha</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Rafael e Felipe B</td>
-	 	 <td>Bruno Damasceno</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Felipe Bernardo</td>
-	 	 <td>Grupo Ágape</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>22/10/17</td>
-	 	 <td>Glaylton</td>
-	 	 <td>Kailane</td>
-	 	 <td>Luiz Fernando</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Janderson e Rafael</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Felipe Paiva</td>
-	 	 <td>Gabriela Almeida</td>
-	 	 <td>Jovens</td>
- 	 </tr>
- 	 <tr>
-	 	 <td>29/10/17</td>
-	 	 <td>Gilvan</td>
-	 	 <td>Victória Almeida</td>
-	 	 <td>Rebeca Hilary</td>
-	 	 <td>Beth e Andréia Paiva</td>
-	 	 <td>Éder e Felipe Bernardo</td>
-	 	 <td>Gabriela Almeida</td>
-	 	 <td>Rafael</td>
-	 	 <td>Beatriz</td>
-	 	 <td>Gurpo Ágape</td>
- 	 </tr>
+	<thead>
+	 	<th>DATA</th>
+	 	<th>SEGURANÇA E PORTARIA</th>
+	 	<th>RECEPÇÃO EXTERNA</th>
+	 	<th>PORTAS, JANELAS E CORTINAS</th>
+	 	<th>RECEPÇÃO INTERNA</th>
+	 	<th>SOM</th>
+	 	<th>DATA-SHOW</th>
+	 	<th>FILMAGEM</th>
+	 	<th>FOTOGRAFIA</th>
+	 	<th>GRUPO DE LOUVOR</th>
+	</thead>
+	<tr>
+	 	<td>5/11/2017</td>
+	 	<td>Glaylton</td>
+	 	<td>Victória Almeida</td>
+	 	<td>Luiz Fernando</td>
+	 	<td>Beth e Andréia Paiva</td>
+	 	<td>Bruno e Janderson</td>
+	 	<td>Letícia</td>
+	 	<td>José Antônio</td>
+	 	<td>Beatriz</td>
+	 	<td>Jovens</td>
+	</tr>
+	<tr>
+	 	<td>12/11/2017</td>
+	 	<td>Gilvan</td>
+	 	<td>Eva</td>
+	 	<td>Rebeca Hilary</td>
+	 	<td>Beth e Andréia Paiva</td>
+	 	<td>Felipe B. e Éder</td>
+	 	<td>Gabriela Almeida</td>
+	 	<td>Janderson</td>
+	 	<td>Felipe Paiva</td>
+	 	<td>Grupo Ágape</td>
+	</tr>
+	<tr>
+	 	<td>19/11/2017</td>
+	 	<td>Glaylton</td>
+	 	<td>Victória Martha</td>
+	 	<td>Luiz Fernando</td>
+	 	<td>Beth e Andréia Paiva</td>
+	 	<td>José Antônio e Rafael</td>
+	 	<td>Beatriz</td>
+	 	<td>Felipe Bernardo</td>
+	 	<td>Felipe Paiva</td>
+	 	<td>Jovens</td>
+	</tr>
+	<tr>
+	 	<td>26/11/2017</td>
+	 	<td>Gilvan</td>
+	 	<td>Isabella</td>
+	 	<td>Rebeca Hilary</td>
+	 	<td>Beth e Andréia Paiva</td>
+	 	<td>Felipe B. e Éder</td>
+	 	<td>Bruno Damasceno</td>
+	 	<td>José Antônio</td>
+	 	<td>Letícia</td>
+	 	<td>Grupo Ágape</td>
+	</tr>
 </table>
+
           </div>
     </div>
 </div>
