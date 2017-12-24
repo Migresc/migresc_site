8f9926d9f607cf00d919f57211474b7765d31025
Bruno Damasceno Martins <bdm2943@gmail.com>
2017-12-24 21:27:02 -0200

    Link para cadastro de membros

--- index.php
+++ index.php
@@ -9,6 +9,15 @@
         Vamos nos juntar em um grupo de estudo? <a href="https://docs.google.com/forms/d/e/1FAIpQLSeyaK1O1V5msp6GmUdWBBT9elXRJ6Xqb626mskq--d-b9T-sw/viewform" target="_blank">Inscreva-se aqui!</a>
     </div>
 </div>
+
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">Cadastro de membros!</h3>
+    </div>
+    <div class="panel-body">
+        Venha você fazer parte da nossa comunidade! <a href="http://migresc.com.br/cadastro" target="_blank">Cadastre-se aqui!</a>
+    </div>
+</div>
 <?php include 'pegi.php';?>
 <?php include 'atividadesNormais.php';?>
 <?php include 'eventos.php';?>
