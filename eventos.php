595e3bae83a221b8fc73531cf2efa806d9dfe467
Bruno Damasceno Martins <bdm2943@gmail.com>
2018-02-06 11:00:55 -0200

    Update

--- eventos.php
+++ eventos.php
@@ -1,13 +1,12 @@
 <div class="panel panel-info">
 	<div class="panel-heading">
-		<h3 class="panel-title">Eventos <a class="glyphicon glyphicon-calendar"></a></h3>
+		<h3 class="panel-title">Eventos<a class="glyphicon glyphicon-calendar"></a></h3>
 	</div>
 	<div class="panel-body">
 		<div class="list-group">
 			<a href="#" class="list-group-item active">
-				<h4 class="list-group-item-heading">Sexta-Feira - 20/10 - Culto de aniversário da IEP Sede</h4>
-				<p class="list-group-item-text">Sairemos da igreja às 19h00, pois o culto em Pinheiros começará às 20h.
-					<br> Confirmem presença</p>
+				<h4 class="list-group-item-heading">Retiro de Jovens - 10-12/02</h4>
+				<p class="list-group-item-text">Sairemos da igreja às 07h do sábado e voltaremos na segunda após o almoço</p>
 			</a>
 		</div>
 	</div>
