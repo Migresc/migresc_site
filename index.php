34463f48d6b58b8b5f18e20c40405f5a1694aff7
Bruno Damasceno Martins <bdm2943@gmail.com>
2018-02-15 21:43:20 -0200

    Nova palavra pastoral

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
         <h3 class="panel-title">Seja bem vindo!</h3>
     </div>
     <div class="panel-body">
+        Você sabe como a igreja e o cristão devem tratar o sexo antes do casamento? <a target="_blank" href="./arquivos/SexoEntraNaoCasados.pdf">Leia mais aqui!</a>
+    </div>
+</div>
+
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3 class="panel-title">Grupo de estudo</h3>
+    </div>
+    <div class="panel-body">
         Vamos nos juntar em um grupo de estudo? <a href="./grupo-estudo.php">Mais informações aqui!</a>
     </div>
 </div>
